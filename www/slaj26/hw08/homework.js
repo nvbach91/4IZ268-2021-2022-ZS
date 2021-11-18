369ea3fd3e6a767720f8fc9f078d7003c6785dea
Jakub Šlambor <63538910+mjamesCZ@users.noreply.github.com>
2021-11-18 09:52:30 +0100

    slaj26-hw08
    Odevzdávám úkol 8.
    https://eso.vse.cz/~slaj26/cv08/

---
+++ www/slaj26/hw08/homework.js
@@ -0,0 +1,215 @@
+/* HOMEWORK */
+/**
+ * 0) Pre-preparacion.
+ * - Vytvořte HTML stránku s nadpisem h1 "JavaScript is awesome!"
+ * - Na stránce vytvořte místo pro umístění jednotlivých spouštěčů úkolů - tlačítek (tj. div, který má id s hodnotou "task-buttons").
+ * - Na stránce vytvořte místo pro výpis výsledků úkolů (div, který má id s hodnotou "result").
+ *
+ * - Připojte tento homework.js soubor k vytvořené HTML stránce pomocí tagu <script> (viz LAB) a vyzkoušejte
+ * console.log('Ahoj světe');
+ */
+console.log('hello world')
+
+/**
+ * 1) Pepe's age. Vypište na konzoli smysluplnou oznamovací větu s věkem Pepy, pokud znáte jeho rok narození,
+ * který je uložený v proměnné a pro výpis použijte zřetězení stringů nebo interpolaci. Pro názvy proměnných
+ * používejte smysluplnou angličtinu.
+ */
+// Solution here
+const birthYear = 2000
+const thisYear = new Date().getFullYear()
+
+console.log(`Pepe is ${thisYear - birthYear} years old.`)
+
+/**
+ * 2) WTF (wow, that's fun). Vypište na konzoli teplotu v Fahrenheiht, pokud znáte teplotu v Celsius, a také naopak.
+ * Formát výpisu je: 20°C =  68°F resp. 68°F = 20°C. Opět používejte proměnné. Výpočet probíhá takto:
+ *     z C na F: vynásobit 9, vydělit 5 a přičíst 32.
+ *     z F na C: odečíst 32, vynásobit 5 a vydělit 9.
+ */
+// Solution here
+const celsius = 24
+const fahrenheiht = 70
+
+const convertCelsiusToFahrenheiht = (c) => {
+  return ((c * 9) / 5 + 32).toFixed(1)
+}
+
+const convertFahrenheihtToCelsius = (f) => {
+  return (((f - 32) * 5) / 9).toFixed(1)
+}
+
+console.log(convertCelsiusToFahrenheiht(celsius) + ' °C')
+console.log(convertFahrenheihtToCelsius(fahrenheiht) + ' °F')
+
+/**
+ * 3) Funkce function fonction funktio. Vemte předchozí úlohy a udělejte z nich funkce. Tj. vytvoříte funkce,
+ * které přijímají argumenty, a na základě argumentů po zavolání vypíše výsledek na konzoli.
+ * Párkrát zavolejte tyto funkce s různými argumenty. V konzoli také vyzkoušejte, zda fungují vaše funkce.
+ *
+ * Pro testování funkce:
+ * - Pouze pomocí JavaScriptu (bez knihoven) vytvořte HTML tlačítko s názvem této úlohy, resp. co funkce dělá, a
+ * id s číslem úlohy <button id="task-1">Uloha 1 (Pepe's age)</button>, umístěte ho na stránku do předem vytvořeného
+ * místa <div id="tasks"></div> a pomocí posluchače události "click" nabindujte implementovanou funkci na toto tlačítko.
+ *
+ * Výsledkem má být tlačítko, na které když kliknete, tak se provede to, co je implementováno ve funkci.
+ *
+ */
+// Solution here
+const getAge = (birthYear) => {
+  return new Date().getFullYear() - birthYear
+}
+
+// Stupně už jsem udělal jako funkce ¯\_(ツ)_/¯
+// Buttons jsou vytvořerny dole
+
+/**
+ * 4) %CENSORED%. Vytvořte funkci, která vezme 2 číselné argumenty a vrátí podíl prvního čísla a druhého čísla.
+ * Výsledek vypište v procentech do předem vytvořeného místa na stránce pro výsledky, např. 21 je 50% z 42. Pro
+ * zkrácení / zaokrouhlení desetinných míst použijte funkci .toFixed(n). Např. var pi = 3.1415926535; pi.toFixed(2);
+ * Pozor na dělení nulou!
+ *
+ * Pro testování vytvořte tlačítko s touto funkcí podle pokynu v úloze č. 3
+ */
+// Solution here
+const getPercentage = (a, b) => {
+  if (b === 0) {
+    return 'Devide by zero exception!'
+  }
+
+  return `${a} is ${((a / b) * 100).toFixed(2)}% of ${b}`
+}
+
+/**
+ * 5) Kdo s koho. Vytvořte funkci, která vezme 2 číselné argumenty a vypíše, který z nich je větší, do předem vytvořeného
+ * místa na strácne. Pokud se čísla rovnají, vypište, že se rovnají.
+ *
+ * Vyzkoušejte funkčnost pro celá čísla, desetinná čísla, zlomky, atd., tj. vytvoříte tlačítko s událostí pro každou
+ * kombinaci argumentů a zkuste ji párkrát zavolat kliknutím na toto tlačítko. Tlačítka vytvářejte podle pokynu v
+ * úloze č. 3. Argumenty pro volání funkce zadávejte staticky.
+ */
+// Solution here
+const compare = (a, b) => {
+  if (a < b) {
+    return b
+  }
+
+  if (a > b) {
+    return a
+  }
+
+  return 'Numbers are equal'
+}
+
+/**
+ * 6) I can cleary see the pattern. Vytvořte funkci, která vypíše popořadě všechny násobky 13, které jsou menší
+ * nebo rovno 730, včetě nuly. Používejte for cyklus.
+ *
+ * Pro testování vytvořte tlačítko s touto funkcí podle pokynu v úloze č. 3.
+ */
+// Solution here
+const pattern = () => {
+  let result = ''
+
+  for (let i = 13; i <= 730; i += 13) {
+    result += i + ' '
+  }
+
+  return result
+}
+
+/**
+ * 7) Around and about. Vytvořte funkci, která vypočte obsah kružnice podle dodaného poloměru v argumentu.
+ *
+ * Pro testování vytvořte tlačítko s touto funkcí podle pokynu v úloze č. 3. Argumenty pro volání funkce zadávejte
+ * staticky.
+ */
+// Solution here
+const getCircleArea = (radius) => {
+  return Math.PI * radius ** 2
+}
+
+/**
+ * 8) Another dimension. Vytvořte funkci, která vypočte objem kuželu, pokud dostanete na argumentech výšku a poloměr.
+ *
+ * Pro testování vytvořte tlačítko s touto funkcí podle pokynu v úloze č. 3. Argumenty pro volání funkce zadávejte
+ * staticky.
+ */
+// Solution here
+const getConeVolume = (height, radius) => {
+  return (Math.PI * radius ** 2 * height) / 3
+}
+
+/**
+ * 9) Not sure if triangle, or just some random values. Vytvořte funkci, která rozhodne, zda se z
+ * dodaných 3 délek na argumentu funkce dá postavit trojúhelník, tj. vypíše tyto 3 délky stran a, b, a c
+ * a výsledek buď ano/ne, true/yes nebo false/no. Z funkce vraťte hodnotu true/false
+ *
+ * Pro testování vytvořte tlačítko s touto funkcí podle pokynu v úloze č. 3. Argumenty pro volání funkce zadávejte
+ * staticky.
+ */
+// Solution here
+const isTriangle = (a, b, c) => {
+  return a + b > c && a + c > b && b + c > a
+}
+
+/**
+ * 10) Heroic performance. Vytvořte funkci, která vypočte a vypíše obsah trojúhelníka podle Heronova vzorce,
+ * tj. funkce dostane délky všech 3 stran. Použijte přitom předchozí validaci v úloze č. 9, tj. počítejte pouze,
+ * když to má smysl. Hint: funkce pro odmocninu je Math.sqrt().
+ *
+ * Pro testování vytvořte tlačítko s touto funkcí podle pokynu v úloze č. 3. Argumenty pro volání funkce zadávejte
+ * staticky.
+ */
+// Solution here
+// - krok 1 - vytvořte funkci
+//   - krok 1.1 - pomocí selektoru vyberte container pro výpis výsledků a uložte ho do proměnné
+//   - krok 1.2 - zvalidujte vstupní argumenty pomocí funkce z úlohy č. 9
+//     - v případě nevalidních hodnot vypište chybovou hlášku na místo pro výpis výsledků a funkci ukončete
+//     - v případě validních hodnot pokračujte s výpočtem
+//   - krok 1.3 - spočítejte obsah trojúhelníku podle Heronovy vzorce a výsledek uložte do proměnné
+//   - krok 1.4 - vypište výsledek na místo pro výpis výsledků
+// - krok 2 - vytvořte tlačítko
+// - krok 3 - nabindujte na toto tlačítko callback, ve kterém zavoláte implementovanou funkci pro výpočet a výpis výsledků
+// - krok 4 - tlačítko umístěte na stránku
+// - krok 5 - otestujte řešení klikáním na tlačítko
+const getTriangleArea = (a, b, c) => {
+  if (!isTriangle(a, b, c)) {
+    return `Not a triangle`
+  }
+
+  const s = (a + b + c) / 2
+  const area = Math.sqrt(s * (s - a) * (s - b) * (s - c))
+
+  return area
+}
+
+// Tests
+
+const taskButtonsEl = document.getElementById('task-buttons')
+const resultEl = document.getElementById('result')
+
+const tasks = []
+tasks.push(getAge(2000))
+tasks.push(convertCelsiusToFahrenheiht(12))
+tasks.push(convertFahrenheihtToCelsius(72))
+tasks.push(getPercentage(135, 167))
+tasks.push(compare(21, 67))
+tasks.push(pattern())
+tasks.push(getCircleArea(5))
+tasks.push(getConeVolume(20, 16))
+tasks.push(isTriangle(23, 6, 18))
+tasks.push(getTriangleArea(23, 6, 18))
+
+const perform = (taskNumber) => {
+  const output = tasks[taskNumber]
+  resultEl.textContent = output
+}
+
+for (let i = 0; i < 10; i++) {
+  const btn = document.createElement('button')
+  btn.textContent = `ÚKOL ${i + 1}`
+  btn.addEventListener('click', () => perform(i))
+
+  taskButtonsEl.appendChild(btn)
+}
