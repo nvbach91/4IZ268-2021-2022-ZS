46991e3a7a9a543554504a2c63852261f8159f0e
tominatorcz <50962055+tominatorcz@users.noreply.github.com>
2022-01-16 17:44:55 +0100

    mikt04-sp2

--- www/mikt04/sp2/assets/js/main.js
+++ www/mikt04/sp2/assets/js/main.js
@@ -27,6 +27,7 @@ $('#main-content-menu .click').click(function(){
   if(index == 1){
     $('#main-content-add').removeClass('non-active');
     $('#main-content-search').addClass('non-active');
+    $('.main-content-results').addClass('non-active');
   }
 });
 
@@ -55,6 +56,8 @@ $('#main-content-infobar-menu .click').click(function(){
   }
 });
 
+// Svatky //
+
 var strDate = $.datepicker.formatDate('dd.mm.yy', new Date());
 $('#date').append(strDate);
 
@@ -70,22 +73,98 @@ $.ajax({
   }
 });
 
-var nameOf = "";
-
-$.ajax({
-  url: "https://api.themoviedb.org/3/search/movie?api_key=ac508bc638f7b6f9435980b8c95da7f2&language=en-US&query=" + nameOf + "&page=1&include_adult=true",
-  type: 'GET',
-  dataType: 'json',
-  success: function(res) {
-    var show = console.log(res.results[0].original_title);
-    //$('#collection-content').append(res.results[0].original_title);        
-  }
-});
+// Movie database API //
 
 $(document).ready(function() {
+
+  const searchForm = $('#search-box');
+  const addForm = $('#add-form');
+  const movieContainer = $('#results');
+  const movieNameInput = $('input[name="search"]');
+
+  searchForm.submit((e)=> {
+    e.preventDefault();
+    movieContainer.html("");
+    $('.main-content-results').removeClass('non-active');
+    const inputValue = movieNameInput.val();
+    const movieName = inputValue;
+
+    for (let i = 0; i < 4; i += 1) {
+      $.ajax({
+        url: "https://api.themoviedb.org/3/search/movie?api_key=ac508bc638f7b6f9435980b8c95da7f2&language=en-US&query=" + movieName + "&page=1&include_adult=true",
+        type: 'GET',
+        dataType: 'json',
+        success: function(res) {
+            const movieData = {
+              name: res.results[i].original_title,
+              image: 'https://image.tmdb.org/t/p/original' + res.results[i].poster_path,
+            };
+        const movie = createMovie(movieData);
+        movieContainer.append(movie);
+        }
+      });
+    }
+  });
+
+
+  const createMovie = (movie) => {
+    const movieContainer = $('<div>').addClass('movie');
+    const movieNameContainer = $('<div>').addClass('movie-name').text(movie.name);
+    const movieImageContainer = $('<img>').addClass('movie-image').attr('src', movie.image).attr('alt', 'movie-image');
+
+    const movieRemoveButton = $('<button>').text('dismiss').click(() => {
+        movieContainer.remove();
+    });
+
+    movieContainer.append(
+      movieNameContainer,
+      movieImageContainer,
+      movieRemoveButton,
+    );
+
+    return movieContainer;
+  };
+  
+
+
+
+
+
+  /**
   $("#button-search").click(function(e){
     e.preventDefault();
     $('#main-content-results').removeClass('non-active');
+
+    const movieNameInput = $('input[name="search"]');
+    const inputValue = movieNameInput.val().trim();
+    const movieNames = inputValue.split(/ +/);
+
+    $.ajax({
+      url: "https://api.themoviedb.org/3/search/movie?api_key=ac508bc638f7b6f9435980b8c95da7f2&language=en-US&query=" + movieNames + "&page=1&include_adult=true",
+      type: 'GET',
+      dataType: 'json',
+      success: function(res) {
+        console.log(res.results[0].original_title);
+        $('#main-content-results').append(res.results[0].original_title);      
+      }
+    });
   }); 
+
+
+///////////////
+    const existingMovieNames = [];
+    const existingMovies = $('.movie');
+    for (let i = 0; i < existingMovies.length; i++) {
+        const existingMovie = existingMovies.eq(i);
+        const existingMovieNameContainer = existingMovie.find('.movie-name');
+        const existingMovieName = existingMovieNameContainer.text();
+        existingMovieNames.push(existingMovieName);
+        console.log(existingMovie, existingMovieNameContainer, existingMovieName);
+    }
+
+  
+   */
+
+
 });
 
