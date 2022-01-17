01afb1ff96dd2eca626243ca5f3185efbd18d741
tominatorcz <50962055+tominatorcz@users.noreply.github.com>
2022-01-17 12:37:59 +0100

    mikt04-sp2

--- www/mikt04/sp2/assets/js/main.js
+++ www/mikt04/sp2/assets/js/main.js
@@ -80,51 +80,117 @@ $(document).ready(function() {
   const searchForm = $('#search-box');
   const addForm = $('#add-form');
   const movieContainer = $('#results');
+  const collectionContainer = $('#collection-content');
   const movieNameInput = $('input[name="search"]');
+  const movie = $('.movie');
+  const jsonInput = [];
+  let searchColl = [];
+  const libraryColl = [];
+
 
   searchForm.submit((e)=> {
     e.preventDefault();
-    movieContainer.html("");
-    $('.main-content-results').removeClass('non-active');
-    const inputValue = movieNameInput.val();
-    const movieName = inputValue;
+    movieContainer.empty();
+    const movieName = movieNameInput.val();
 
-    for (let i = 0; i < 4; i += 1) {
-      $.ajax({
-        url: "https://api.themoviedb.org/3/search/movie?api_key=ac508bc638f7b6f9435980b8c95da7f2&language=en-US&query=" + movieName + "&page=1&include_adult=true",
-        type: 'GET',
-        dataType: 'json',
-        success: function(res) {
-            const movieData = {
-              name: res.results[i].original_title,
-              image: 'https://image.tmdb.org/t/p/original' + res.results[i].poster_path,
-            };
-        const movie = createMovie(movieData);
-        movieContainer.append(movie);
+    $.ajax({
+      url: "https://api.themoviedb.org/3/search/movie?api_key=ac508bc638f7b6f9435980b8c95da7f2&language=en-US&query=" + movieName + "&page=1&include_adult=true",
+      type: 'GET',
+      dataType: 'json',
+      success: function(res) {
+        $('.main-content-results').removeClass('non-active');
+        searchColl.length = 0;
+        for (let i = 0; i < 4; i += 1) {
+          const movieData = {
+            id: res.results[i].id,
+            name: res.results[i].original_title,
+            year: res.results[i].release_date,
+            image: 'https://image.tmdb.org/t/p/original' + res.results[i].poster_path,
+          };
+          searchColl.push(movieData);
         }
-      });
-    }
+        showMovie(searchColl);
+        console.log(searchColl);
+      }
+    });
+      //const movie = createMovie(movieData);
+      //movieContainer.append(movie);
   });
+
+  function insertMovie(movie) {
+    let idExists = false;
+    libraryColl.forEach(function(element) {
+      if (element.id == movie.id) {
+        idExists = true;
+      }
+    });
+    if (idExists == false) {
+      libraryColl.push(movie);
+      collectionContainer.append(addMovie(movie));
+      return true;
+    }
+  }
+
+  /*
+  const insertMovie = (movieData1) => {
+    libraryColl.forEach(element => {
+      if(element.id === movieData1.id){
+        console.log('Chyba');
+        return false;
+      }
+    });
+    libraryColl.push(movieData1);
+    console.log(libraryColl);
+  }
+  */
+
+  const showMovie = (collection) => {
+    collection.forEach(function(element) {
+      movieContainer.append(createMovie(element));
+    });
+  }
 
 
   const createMovie = (movie) => {
-    const movieContainer = $('<div>').addClass('movie');
-    const movieNameContainer = $('<div>').addClass('movie-name').text(movie.name);
-    const movieImageContainer = $('<img>').addClass('movie-image').attr('src', movie.image).attr('alt', 'movie-image');
-
-    const movieRemoveButton = $('<button>').text('dismiss').click(() => {
-        movieContainer.remove();
+    const movieContainer = $('<div>').addClass('movie-result');
+    const movieNameContainer = $('<div>').addClass('movie-result-name').text(movie.name);
+    const movieImageContainer = $('<img>').addClass('movie-result-image').attr('src', movie.image).attr('alt', 'movie-image');
+    const movieMessageContainer = $('<div>').addClass('movie-result-message').text('Film byl přidán');
+    
+    const movieAddButton = $('<button>').text('Přidat').click(() => {
+      insertMovie(movie);
+      console.log(libraryColl);
+      movieAddButton.remove();
+      movieContainer.append(movieMessageContainer);
     });
 
     movieContainer.append(
       movieNameContainer,
       movieImageContainer,
-      movieRemoveButton,
+      movieAddButton,
     );
 
     return movieContainer;
   };
-  
+
+  const addMovie = (movie) => {
+    const movieContainer = $('<div>').addClass('movie');
+    const movieNameContainer = $('<div>').addClass('movie-name').text(movie.name);
+    const movieImageContainer = $('<img>').addClass('movie-image').attr('src', movie.image).attr('alt', 'movie-image');
+    
+    const movieAddButton = $('<button>').text('Odebrat').click(() => {
+      movieContainer.remove();
+    });
+
+    movieContainer.append(
+      movieNameContainer,
+      movieImageContainer,
+      movieAddButton,
+    );
+
+    return movieContainer;
+  };
+
 
 
 
