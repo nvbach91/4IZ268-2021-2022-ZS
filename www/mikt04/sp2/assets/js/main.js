b254069b12d97d7860fd077abfd197a6af516bee
tominatorcz <50962055+tominatorcz@users.noreply.github.com>
2022-01-17 16:37:13 +0100

    mikt04-sp2
    Přidáno napojení na popis filmu v knihovně.

--- www/mikt04/sp2/assets/js/main.js
+++ www/mikt04/sp2/assets/js/main.js
@@ -1,11 +1,7 @@
-const data = [
-  'Content 1', 'Content 2',
-];
-
 /** MAIN CONTENT MENU - SEARCH ADD CLICK */
-$('#main-content-menu .click').click(function(){
-  if($(this).hasClass('active')){
-      return;
+$('#main-content-menu .click').click(function () {
+  if ($(this).hasClass('active')) {
+    return;
   }
 
   var index = $(this).attr('data-content');
@@ -15,16 +11,16 @@ $('#main-content-menu .click').click(function(){
       $(this).text(data[index]).fadeIn(200);
   });
    */
-  
+
   $('#main-content-menu .click').removeClass('active');
   $(this).addClass('active');
 
-  if (index == 0){
+  if (index == 0) {
     $('#main-content-search').removeClass('non-active');
     $('#main-content-add').addClass('non-active');
   }
 
-  if(index == 1){
+  if (index == 1) {
     $('#main-content-add').removeClass('non-active');
     $('#main-content-search').addClass('non-active');
     $('.main-content-results').addClass('non-active');
@@ -32,94 +28,112 @@ $('#main-content-menu .click').click(function(){
 });
 
 /**MAIN CONTENT INFOBOX - SEARCH ADD CLICK */
-$('#main-content-infobar-menu .click').click(function(){
+$('#main-content-infobar-menu .click').click(function () {
   var index = $(this).attr('data-content');
 
   $('#main-content-infobar-menu .click').removeClass('active');
   $(this).addClass('active');
 
-  if (index == 0){
+  if (index == 0) {
     $('#main-content-infobar-info').removeClass('non-active');
     $('#main-content-infobar-random').addClass('non-active');
     $('#main-content-infobar-really').addClass('non-active');
   }
 
-  if(index == 1){
+  if (index == 1) {
     $('#main-content-infobar-random').removeClass('non-active');
     $('#main-content-infobar-info').addClass('non-active');
     $('#main-content-infobar-really').addClass('non-active');
   }
-  if(index == 2){
+  if (index == 2) {
     $('#main-content-infobar-really').removeClass('non-active');
     $('#main-content-infobar-random').addClass('non-active');
     $('#main-content-infobar-info').addClass('non-active');
   }
 });
 
-// Svatky //
 
-var strDate = $.datepicker.formatDate('dd.mm.yy', new Date());
-$('#date').append(strDate);
+$(document).ready(function () {
 
-$.ajax({
-  url: "https://svatky.adresa.info/json",
-  type: 'GET',
-  dataType: 'json',
-  success: function(res) {
-    var parsed =  JSON.parse(JSON.stringify(res));
-    $.each(parsed, function (key, val) {
-      $('#svatek').append(val.name);             
-    });
-  }
-});
+  // Svatky //
 
-// Movie database API //
+  var strDate = $.datepicker.formatDate('dd.mm.yy', new Date());
+  var strYear = $.datepicker.formatDate('yy', new Date());
+  $('#date').append(strDate);
+  $('#year').append(strYear);
 
-$(document).ready(function() {
+  $.ajax({
+    url: "https://svatky.adresa.info/json",
+    type: 'GET',
+    dataType: 'json',
+    success: function (res) {
+      var parsed = JSON.parse(JSON.stringify(res));
+      $.each(parsed, function (key, val) {
+        $('#svatek').append(val.name);
+      });
+    }
+  });
+
+  // Movie database API //
 
   const searchForm = $('#search-box');
-  const addForm = $('#add-form');
+  const addForm = $('#add-box');
   const movieContainer = $('#results');
   const collectionContainer = $('#collection-content');
   const movieNameInput = $('input[name="search"]');
-  const movie = $('.movie');
   const jsonInput = [];
   let searchColl = [];
   const libraryColl = [];
 
+  const showSpinner = () => {
+    const spinner = $('<div>').addClass('spinner');
+    searchForm.append(spinner);
+    console.log(spinner);
+    return spinner;
+  };
 
-  searchForm.submit((e)=> {
+
+  searchForm.submit((e) => {
     e.preventDefault();
     movieContainer.empty();
     const movieName = movieNameInput.val();
-
+    spinner = showSpinner();
     $.ajax({
-      url: "https://api.themoviedb.org/3/search/movie?api_key=ac508bc638f7b6f9435980b8c95da7f2&language=en-US&query=" + movieName + "&page=1&include_adult=true",
+      statusCode: {
+        500: function() {
+          
+        }
+      },
+      url: "https://api.themoviedb.org/3/search/movie?api_key=ac508bc638f7b6f9435980b8c95da7f2&language=cs-CZ&query=" + movieName + "&page=1&include_adult=true",
       type: 'GET',
       dataType: 'json',
-      success: function(res) {
-        $('.main-content-results').removeClass('non-active');
+      success: function (res) {
         searchColl.length = 0;
         for (let i = 0; i < 4; i += 1) {
           const movieData = {
             id: res.results[i].id,
             name: res.results[i].original_title,
+            description: res.results[i].overview,
             year: res.results[i].release_date,
             image: 'https://image.tmdb.org/t/p/original' + res.results[i].poster_path,
           };
           searchColl.push(movieData);
         }
+        $('.main-content-results').removeClass('non-active');
+        spinner.remove();
         showMovie(searchColl);
         console.log(searchColl);
+      },
+      error: function (data) {
+        console.log('ajax error' + data);
       }
     });
-      //const movie = createMovie(movieData);
-      //movieContainer.append(movie);
+
   });
 
   function insertMovie(movie) {
     let idExists = false;
-    libraryColl.forEach(function(element) {
+    libraryColl.forEach(function (element) {
       if (element.id == movie.id) {
         idExists = true;
       }
@@ -131,32 +145,23 @@ $(document).ready(function() {
     }
   }
 
-  /*
-  const insertMovie = (movieData1) => {
-    libraryColl.forEach(element => {
-      if(element.id === movieData1.id){
-        console.log('Chyba');
-        return false;
-      }
-    });
-    libraryColl.push(movieData1);
-    console.log(libraryColl);
+  function deleteMovie(movie) {
+    libraryColl.splice(libraryColl.findIndex(element => element.id == movie.id), 1);
   }
-  */
 
   const showMovie = (collection) => {
-    collection.forEach(function(element) {
+    collection.forEach(function (element) {
       movieContainer.append(createMovie(element));
     });
   }
 
-
+  // Creates movie html structure for search sellection
   const createMovie = (movie) => {
     const movieContainer = $('<div>').addClass('movie-result');
     const movieNameContainer = $('<div>').addClass('movie-result-name').text(movie.name);
     const movieImageContainer = $('<img>').addClass('movie-result-image').attr('src', movie.image).attr('alt', 'movie-image');
     const movieMessageContainer = $('<div>').addClass('movie-result-message').text('Film byl přidán');
-    
+
     const movieAddButton = $('<button>').text('Přidat').click(() => {
       insertMovie(movie);
       console.log(libraryColl);
@@ -173,13 +178,21 @@ $(document).ready(function() {
     return movieContainer;
   };
 
+  // creates movie html structure for library collection
   const addMovie = (movie) => {
-    const movieContainer = $('<div>').addClass('movie');
+    const infoBox = $('#info-box');
+    const movieContainer = $('<div>').addClass('movie').click(() => {
+      infoBox.empty();
+      infoBox.append(addMovieInfo(movie));
+      $('.movie').removeClass('active-movie');
+      movieContainer.addClass('active-movie');
+    });
     const movieNameContainer = $('<div>').addClass('movie-name').text(movie.name);
     const movieImageContainer = $('<img>').addClass('movie-image').attr('src', movie.image).attr('alt', 'movie-image');
-    
+
     const movieAddButton = $('<button>').text('Odebrat').click(() => {
       movieContainer.remove();
+      deleteMovie(movie);
     });
 
     movieContainer.append(
@@ -191,46 +204,38 @@ $(document).ready(function() {
     return movieContainer;
   };
 
+  // creates movie html structure for movie information
+  const addMovieInfo = (movie) => {
+    const movieInfoContainer = $('<div>').addClass('movie-info');
+    const movieInfoDescriptionContainer = $('<div>').addClass('movie-description').text(movie.description);
 
+    movieInfoContainer.append(
+      movieInfoDescriptionContainer,
+    );
+
+    return movieInfoContainer;
+  };
+
+  // active library 
+  /*
+  const movie = $(document).on('click', '.movie', function(){
+    $(".movie").removeClass("active-movie");
+    $(this).addClass("active-movie");
+    console.log(this);
+  });
+  */
+
+  addForm.submit((e) => {
+    e.preventDefault();
+
+  });
 
 
 
 
   /**
-  $("#button-search").click(function(e){
-    e.preventDefault();
-    $('#main-content-results').removeClass('non-active');
 
-    const movieNameInput = $('input[name="search"]');
-    const inputValue = movieNameInput.val().trim();
-    const movieNames = inputValue.split(/ +/);
-
-    $.ajax({
-      url: "https://api.themoviedb.org/3/search/movie?api_key=ac508bc638f7b6f9435980b8c95da7f2&language=en-US&query=" + movieNames + "&page=1&include_adult=true",
-      type: 'GET',
-      dataType: 'json',
-      success: function(res) {
-        console.log(res.results[0].original_title);
-        $('#main-content-results').append(res.results[0].original_title);      
-      }
-    });
-  }); 
-
-
-///////////////
-    const existingMovieNames = [];
-    const existingMovies = $('.movie');
-    for (let i = 0; i < existingMovies.length; i++) {
-        const existingMovie = existingMovies.eq(i);
-        const existingMovieNameContainer = existingMovie.find('.movie-name');
-        const existingMovieName = existingMovieNameContainer.text();
-        existingMovieNames.push(existingMovieName);
-        console.log(existingMovie, existingMovieNameContainer, existingMovieName);
-    }
-
-  
    */
 
 
 });
-
