3770dd9d8e23d531dfa27ac84efe199c5721d983
slie00 <92124680+slie00@users.noreply.github.com>
2022-01-20 11:40:49 +0100

    Add files via upload
    final

--- www/slie00/sp2/src/App.js
+++ www/slie00/sp2/src/App.js
@@ -1,74 +1,90 @@
-import './App.css';
 import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import Item from "./Item";
-import {initializeApp} from 'firebase/app';
-import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth';
-import {getDocs, orderBy, query, collection, getFirestore, connectFirestoreEmulator, addDoc} from 'firebase/firestore';
-import {connectStorageEmulator, getStorage, uploadBytes, ref, getDownloadURL, deleteObject} from 'firebase/storage'
+import {initializeApp} from "firebase/app";
+import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from "firebase/auth";
+import {getDocs, doc, orderBy, query, updateDoc, collection, getFirestore, addDoc, where} from "firebase/firestore";
+import {getStorage, uploadBytes, ref, getDownloadURL, deleteObject} from "firebase/storage"
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDa6GnHydWGTKGuVTepwBFqYQE5O8YPcw4",
-	authDomain: "marketplace-6f10d.firebaseapp.com",
-	projectId: "marketplace-6f10d",
-	storageBucket: "marketplace-6f10d.appspot.com",
-	messagingSenderId: "27510960861",
-	appId: "1:27510960861:web:0c026a41421bda2a142134",
-	measurementId: "G-WM1LB2XW7L"
-};
+	apiKey: "AIzaSyDgDQg3Arvw60E0Nlyt445WugFiK9JSc0k",
+	authDomain: "webslie00.firebaseapp.com",
+	projectId: "webslie00",
+	storageBucket: "webslie00.appspot.com",
+	messagingSenderId: "664230584819",
+	appId: "1:664230584819:web:770e6e8a0a6fd62a053286",
+	measurementId: "G-109J05KX1M"
+  };
 
 const app = initializeApp(firebaseConfig);
-connectFirestoreEmulator(getFirestore(app), 'localhost', 8080);
-connectStorageEmulator(getStorage(app), 'localhost', 9199);
 
 function ImageEditBox({image, deleteImage}) {
 	const [remove, setRemove] = useState(false);
-	return (
-		<div className={'border border-2 border-black relative'} key={image.id} onMouseEnter={() => setRemove(true)}
-		     onMouseLeave={() => setRemove(false)}>
-			<img className={'w-full h-32 object-cover'} src={image.url} alt={image.title}/>
-			{remove && <div className={'w-full h-full absolute top-0 left-0 bg-black opacity-50'}>
-				<span className="material-icons text-white cursor-pointer text-3xl" onClick={deleteImage}>close</span>
-			</div>}
-		</div>
-	)
+
+	return (<div className={"border border-2 border-black relative"} key={image.id} onMouseEnter={() => setRemove(true)}
+	             onMouseLeave={() => setRemove(false)}>
+		<img className={"w-full h-32 object-cover"} src={image.url} alt={image.title}/>
+		{remove && <div className={"w-full h-full absolute top-0 left-0 bg-black opacity-50"}>
+			<span className="material-icons text-white cursor-pointer text-3xl" onClick={deleteImage}>close</span>
+		</div>}
+	</div>)
 }
 
-function App(props) {
+function App() {
 	const [user, setUser] = useState(null)
-	const [tab, setTab] = useState('home')
+	const [tab, setTab] = useState("home")
 	const [items, setItems] = useState([])
-	const [modal, setModal] = useState(true)
-	const [title, setTitle] = useState('')
-	const [desc, setDesc] = useState('')
+	const [modal, setModal] = useState(false)
+
+	const [spinner, setSpinner] = useState(true)
+
+	const [title, setTitle] = useState("")
+	const [desc, setDesc] = useState("")
+	const [image, setImage] = useState(null)
 	const [price, setPrice] = useState(0)
-	const [images, setImages] = useState([])
-	const [update, setUpdate] = useState(null)
+
+	const [update, setUpdate] = useState(null) // id of item to update
+	const [error, setError] = useState(null)
+	const [loading, setLoading] = useState(false)
+
 	useEffect(() => {
+		// calls when application is loaded
 		getAuth(app).onAuthStateChanged(user => {
 			setUser(user)
 		});
-		(async () => {
-			const data = await getDocs(query(collection(getFirestore(app), 'items'), orderBy('createdAt', 'desc')))
-			setItems(data.docs.map(e => ({...e.data(), id: e.id})))
-		})();
+		refetch()
 	}, [])
 
-	const onTabChange = tab => {
-		setTab(tab)
+	async function refetch() {
+		if (tab === "mine" && user !== null) {
+			const data = await getDocs(query(collection(getFirestore(app), "items"), where("user", "==", user.uid), orderBy("createdAt", "desc")))
+			setItems(data.docs.map(e => ({...e.data(), id: e.id})))
+		} else {
+			const data = await getDocs(query(collection(getFirestore(app), "items"), orderBy("createdAt", "desc")))
+			console.log(data);
+			setItems(data.docs.map(e => ({...e.data(), id: e.id})))
+		}
+		setSpinner(false)
 	}
-	const signIn = () => {
+
+	async function onTabChange(tab) {
+		setTab(tab)
+		await refetch()
+	}
+
+	function signIn() {
 		signInWithPopup(getAuth(app), new GoogleAuthProvider())
 			.then(result => {
 				setUser(result.user)
 			})
 	}
-	const logOut = async () => {
+
+	async function logOut() {
 		await signOut(getAuth(app))
 	}
 
-	const handleTitleChange = e => {
-		setTitle(e.target.value)
+	const handleTitleChange = event => {
+		setTitle(event.target.value)
 	}
 	const handleDescChange = e => {
 		setDesc(e.target.value)
@@ -77,103 +93,150 @@ function App(props) {
 		setPrice(Number(e.target.value) || 0)
 	}
 	const handleImageInput = async e => {
-		let imgs = []
-		for (let image of e.target.files) {
-			let id = new Date().toISOString()
-			let r = ref(getStorage(app), id)
-			let result = await uploadBytes(r, image)
-			console.log(result)
-			imgs.push({ref: result.ref, id: id, url: await getDownloadURL(r)})
+		if (!e.target.files[0].type.startsWith("image/")) {
+			return setError("File must be an image")
 		}
-		setImages(images => [...images, ...imgs].splice(0, 6))
+		let id = "images/" + new Date().toISOString()
+		let r = ref(getStorage(app), id)
+		let result = await uploadBytes(r, e.target.files[0])
+		let img = {ref: result.ref, id: id, url: await getDownloadURL(r)}
+		setImage(img)
 		e.target.value = null
+		return setError(null)
 	}
 
-	const deleteImage = async (id) => {
+	async function deleteImage(id) {
 		await deleteObject(ref(getStorage(app), id))
-		setImages(images => images.filter(e => e.id !== id))
+		setImage(null)
 	}
 
-	const save = async () => {
-		let item = {
-			title,
-			desc,
-			price,
-			images: images.map(i => i.id),
-			createdAt: new Date().toISOString(),
-			user: user.uid
+	async function save(e) {
+		e.preventDefault() //  for not sending form(do not reload page)
+
+		if (image === null) {
+			return setError("Please add an image");
 		}
-
-		addDoc(getFirestore(app), 'items').set(item)
-		setTitle('')
-		setDesc('')
+		if (!title) {
+			return setError("Please fill the title")
+		}
+		setError(null)
+		setLoading(true)
+		let item = {
+			title, desc, price, image: image.id, createdAt: new Date().toISOString(), user: user.uid
+		}
+		if (update !== null) {
+			await updateDoc(doc(getFirestore(app), "items", update), item)
+		} else {
+			await addDoc(collection(getFirestore(app), "items"), item)
+		}
+		setTitle("")
+		setDesc("")
 		setPrice(0)
-		setImages([])
+		setImage(null)
+		setLoading(false);
+		await refetch()
+		setUpdate(null)
 		setModal(false)
+		return true;
 	}
 
-	return (
-		<main>
-			<nav className="p-2 bordered border-2 full-width flex justify-between rounded-2xl m-2">
-				<div className="p-1">
-					<Link to="/" onClick={() => onTabChange('home')} className="font-bold p-2 text-2xl">Home</Link>
-					{user &&
-						<Link to="/items" onClick={() => onTabChange('mine')} className="font-bold p-2 text-2xl">My Items</Link>}
-				</div>
-				{user ?
-					<button className='bg-red-500 text-white font-bold py-2 px-4 rounded-xl w-28'
-					        onClick={logOut}>Log out</button>
-					:
-					<button className='bg-blue-500 text-white font-bold py-2 px-4 rounded-xl' onClick={signIn}>Sign In</button>
-				}
-			</nav>
-			<div className={'flex flex-row px-12'}>
-				{items.map(item => {
-					return (<Item item={item} key={item.id}/>)
-				})}
+	async function edit(item) {
+		try {
+			setImage({id: item.image, url: await getDownloadURL(ref(getStorage(app), item.image)).catch(undefined)})
+		} catch (e) {
+			setImage(null)
+		}
+		setTitle(item.title)
+		setDesc(item.desc)
+		setPrice(item.price)
+		setUpdate(item.id)
+		setModal(true);
+	}
+
+	function cancel(e) {
+		e.preventDefault()
+		setTitle("")
+		setDesc("")
+		setPrice(0)
+		setImage(null)
+		setModal(false)
+		setLoading(false);
+		setUpdate(null)
+		return true;
+	}
+
+	return (<main className="overflow-auto max-h-screen">
+		<nav className="p-2 bordered border-2 full-width flex justify-between rounded-2xl m-2 ">
+			<div className="p-1">
+				<Link to="/" onClick={() => onTabChange("home")} className="font-bold p-2 text-2xl">Home</Link>
+				{user !== null &&
+					<Link to="/mine" onClick={() => onTabChange("mine")} className="font-bold p-2 text-2xl">My Items</Link>}
 			</div>
-			<button className='bg-blue-500 text-white font-bold py-2 px-4 rounded-xl fixed bottom-4 right-4 w-28'>Create
-			</button>
+			{user !== null ?
+				<button className="bg-red-500 text-white font-bold py-2 px-4 rounded-xl w-28" onClick={logOut}>Log out</button>
+				:
+				<button className="bg-blue-500 text-white font-bold py-2 px-4 rounded-xl" onClick={signIn}>Sign In</button>}
+		</nav>
+		{spinner && <div className=" z-50 fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+			<div
+				className="animate-spin w-32 h-32 border-x-8 border-blue-800 rounded-full bg-orange-300 flex justify-center items-center">
+				<div className="animate-spin w-16 h-16 rounded-full bg-blue-800"/>
+			</div>
+		</div>}
 
-			{modal && <div className="fixed top-0 left-0 w-full h-full bg-gray-900 opacity-75">
-				<form className='rounded-2xl border-2 border-black w-3/5 h-4/8 p-6 mx-auto my-24 bg-white overflow-auto '>
-					<h1 className={'text-2xl text-center'}>Create new item</h1>
+		<div className={"flex flex-row w-full flex-wrap items-start"}>
+			{items.map(item => <Item item={item} key={item.id} user={user} update={refetch} edit={() => edit(item)}/>)}
+		</div>
+		{items.length === 0 && <div className="p-2 text-center">No items yet</div>}
 
-					<label>
-						<span>Title</span>
-						<input type="text" onChange={handleTitleChange} name="title" value={title}
-						       className={'border border-1 border-black w-full rounded p-2 mb-2'}/>
+		{user &&
+			<button className="bg-blue-500 text-white font-bold py-2 px-4 mx-2 rounded-xl fixed bottom-4 right-4 w-28"
+			        onClick={() => setModal(true)}>
+				Create
+			</button>}
 
-					</label>
-					<label>
-						<span>Description</span>
-						<textarea className={'border border-1 border-black w-full rounded p-2 mb-1'} cols="30" rows="5" value={desc}
-						          onChange={handleDescChange}/>
-					</label>
-					<label>
-						<span>Price($)</span>
-						<input type="number" onChange={handlePriceChange} name="title" value={price}
-						       className={'border border-1 border-black w-full rounded p-2 mb-2'}/>
-					</label>
-					<label>
-						<span>Images</span>
-						<br/>
-						{images.length < 6 ?
-							<input type="file" id="files" name="files" accept="image/png, image/gif, image/jpeg" multiple
-							       onChange={handleImageInput}/> : 'You can have up to 6 images'}
-					</label>
-					<div className={'grid grid-cols-3 gap-2 my-2'}>
-						{images.map(e => <ImageEditBox image={e} key={e.id} deleteImage={() => deleteImage(e.id)}/>)}
-					</div>
+		{modal && <div className="fixed top-0 left-0 w-full h-full bg-gray-900 z-50">
+			<form className="rounded-2xl border-2 border-black w-3/5 h-4/8 p-6 mx-auto my-24 bg-white overflow-auto "
+			      onSubmit={save}>
+				<h1 className={"text-2xl text-center"}>{update !== null ? "Update item" : "Create new item"}</h1>
+				{error && <div className="text-xl bg-red-700 rounded-xl my-4 p-4 text-white">{error}</div>}
 
-					<div className={'flex flex-row justify-between'}>
-						<button className='bg-red-500 w-32 rounded-xl p-2 text-white'>Cancel</button>
-						<button className='bg-blue-500 w-32 rounded-xl p-2 text-white' onClick={save}>Save</button>
-					</div>
-				</form>
-			</div>}
-		</main>
-	);
+				<label>
+					<span>Title</span>
+					<input type="text" onChange={handleTitleChange} name="title" value={title}
+					       className={"border border-1 border-black w-full rounded p-2 mb-2"} required/>
+
+				</label>
+				<label>
+					<span>Description(Optional)</span>
+					<textarea className={"border border-1 border-black w-full rounded p-2 mb-1"} cols="30" rows="5" value={desc}
+					          onChange={handleDescChange}/>
+				</label>
+				<label>
+					<span>Price($)</span>
+					<input type="number" onChange={handlePriceChange} name="title" value={price}
+					       className={"border border-1 border-black w-full rounded p-2 mb-2"} required/>
+				</label>
+				<label>
+					<span>Image</span>
+					<br/>
+					{!image && <input type="file" id="files" name="files" accept="image/*"
+					                  onChange={handleImageInput}/>}
+				</label>
+				{image && <div className={"grid grid-cols-3 gap-2 my-2"}>
+					<ImageEditBox image={image} deleteImage={() => deleteImage(image.id)}/>
+				</div>}
+
+				<div className={"flex flex-row justify-between"}>
+					<button className="bg-red-500 w-32 rounded-xl p-2 text-white" type="button"
+					        onClick={cancel}>Cancel
+					</button>
+					<button className="bg-blue-500 w-32 rounded-xl p-2 text-white"
+					        disabled={loading}>{update ? "Update" : "Save"}</button>
+				</div>
+			</form>
+		</div>}
+	</main>);
 }
 
 export default App;
--- www/slie00/sp2/src/Item.jsx
+++ www/slie00/sp2/src/Item.jsx
@@ -1,7 +1,140 @@
+import {useEffect, useState} from "react";
+import {getDownloadURL, getStorage, ref} from "firebase/storage";
+import {getAuth} from "firebase/auth";
+import {addDoc, collection, deleteDoc, doc, getDocs, getFirestore} from "firebase/firestore";
+
 export default function Item(props) {
-	return (
-		<div className=''>
-			{props.item.name}
+	const [url, setUrl] = useState("");
+	const [currentUser, setCurrentUser] = useState({});
+	const [message, setMessage] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [contacts, setContacts] = useState([]);
+	const [modalContacts, setModalContacts] = useState(false);
+	const [modal, setModal] = useState(false);
+
+
+	useEffect(() => {
+		setCurrentUser((getAuth().currentUser || {}))
+		getDownloadURL(ref(getStorage(), props.item.image)).then(setUrl).catch(undefined);
+	}, [props])
+
+	const {title, desc, image, price, id, user} = props.item;
+
+	async function contact(e) {
+		e.preventDefault()
+		setLoading(true);
+		await addDoc(collection(getFirestore(), "items", id, "contacts"), {
+			email: currentUser.email, name: currentUser.displayName, message, date: new Date().toISOString()
+		});
+		setModal(false);
+		setMessage("");
+		setLoading(false);
+	}
+
+	async function see() {
+		let data = await getDocs(collection(getFirestore(), "items", id, "contacts"));
+		setContacts(data.docs.map(doc => doc.data()))
+		setModalContacts(true)
+	}
+
+	function edit() {
+		props.edit()
+	}
+
+	async function remove() {
+		await deleteDoc(doc(getFirestore(), "items", id));
+		console.log("removed")
+		props.update()
+	}
+
+	function handleMessageInput(e) {
+		setMessage(e.target.value);
+	}
+
+	function closeModal() {
+		setModalContacts(false)
+		setLoading(false)
+	}
+
+	return (<div className="m-2 item-box bordered border-2 rounded-xl h-96">
+		<div className="relative m-0 flex bg-white flex-col w-full flex flex-row justify-between h-full">
+			<div className="flex-no-shrink w-full">
+				<img alt={image} className="block mx-auto w-auto h-48 object-cover" src={url}/>
+			</div>
+			<div className="flex card-block relative">
+				<div className="p-2">
+					<h4 className="font-medium text-2xl mb-3">{title}</h4>
+					<p className="leading-normal">{desc}</p>
+					<p className="text-sm text-grey block mt-6">Price: {price} $</p>
+				</div>
+			</div>
+			{currentUser.email && currentUser.uid !== user &&
+				<button className="bordered border-2 border-black m-1 p-2 rounded-xl bg-green-500"
+				        onClick={() => setModal(true)}>
+					Contact</button>}
+			{currentUser.uid === user && <div className="flex flex-row w-full">
+				<button className={"bordered border-2 border-black m-1 p-2 rounded-xl bg-blue-500 w-1/3"}
+				        onClick={see}>See applicants
+				</button>
+				<button className={"w-1/3 bordered border-2 border-black m-1 p-2 rounded-xl bg-yellow-500"}
+				        onClick={edit}>Edit
+				</button>
+				<button className={"w-1/3 bordered border-2 border-black m-1 p-2 rounded-xl bg-red-500"}
+				        onClick={remove}>Delete
+				</button>
+			</div>}
 		</div>
-	)
+		{modalContacts === true && <div className="fixed top-0 left-0 w-full h-full bg-gray-900 z-50">
+			<div className="rounded-2xl border-2 border-black w-3/5 h-4/8 p-6 mx-auto my-24 bg-white overflow-auto ">
+				<h2 className="text-2xl">Applicants</h2>
+				{contacts.length > 0 ? <table className="w-full">
+					<thead>
+					<tr>
+						<th>E-mail</th>
+						<th>Name</th>
+						<th>Message</th>
+						<th>Created</th>
+					</tr>
+					</thead>
+					<tbody className="text-center">
+					{contacts.map(c => <tr>
+						<td>{c.email}</td>
+						<td>{c.name}</td>
+						<td>{c.message}</td>
+						<td>{new Date(c.date).toLocaleDateString() + " " + new Date(c.date).toLocaleTimeString()}</td>
+					</tr>)}
+					</tbody>
+
+				</table> : <p>No applicants yet</p>}
+				<button className="bordered border-2 border-black p-2 w-1/3 mt-6 rounded-xl bg-green-500"
+				        onClick={closeModal}>
+					Close
+				</button>
+			</div>
+		</div>}
+
+		{modal && <div className="fixed top-0 left-0 w-full h-full bg-gray-900 z-50">
+			<form className="rounded-2xl border-2 border-black w-3/5 h-4/8 p-6 mx-auto my-24 bg-white overflow-auto "
+			      onSubmit={contact}>
+				<h1 className={"text-3xl text-center"}>Leave a contact</h1>
+				<label>
+					<span>E-mail</span>
+					<input type="text" disabled name="email" value={currentUser.email}
+					       className={"border border-1 border-black w-full rounded p-2 mb-2"} required/>
+				</label>
+				<label>
+					<span>Message</span>
+					<textarea className={"border border-1 border-black w-full rounded p-2 mb-1"} cols="30" rows="5"
+					          value={message}
+					          onChange={handleMessageInput}/>
+				</label>
+				<div className={"flex flex-row justify-between"}>
+					<button className="bg-red-500 w-32 rounded-xl p-2 text-white" type="button"
+					        onClick={() => setModal(false)}>Cancel
+					</button>
+					<button className="bg-blue-500 w-32 rounded-xl p-2 text-white" disabled={loading}>Save</button>
+				</div>
+			</form>
+		</div>}
+	</div>)
 }
