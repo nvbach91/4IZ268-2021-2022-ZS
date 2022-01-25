b71cc6f9a655fdfe9993fb9989c4044ef4fb3706
vesl05 <79693886+vesl05@users.noreply.github.com>
2022-01-25 15:06:43 +0100

    version 0.2
    css changed
    add proper controls
    add option to add points on map

--- www/vesl05/sp2/js/app.js
+++ www/vesl05/sp2/js/app.js
@@ -1,9 +1,11 @@
 const map = L.map('map').setView([50.080, 14.451], 15);
 
-const popup = L.popup()
-    .setLatLng([50.084559, 14.441183])
-    .setContent("Vysoká škola ekonomická")
-    .openOn(map);
+var myMarkerGroup = L.featureGroup().addTo(map).on("click", markersClick);
+
+var mapPoints = []
+
+var marker = L.marker([50.084559, 14.441183]).addTo(map);
+marker.bindPopup("<b>Bod na mapě</b><br>To to je VŠE").openPopup();
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -14,9 +16,45 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoibHVrYW5lIiwiYSI6ImNreGo3cXp6dDZiaTMyeGxhZXkyaGltcnIifQ.uLQYOB-Oo_szmnDn8WwnCw'
 }).addTo(map);
 
-function addPoint() {
+function onMapClick(e) {
 
-    console.log("Test")
+    var marker = L.marker(e.latlng)
 
-    return true
+    if($("#add").is(":checked"))
+    {
+        var uniqId = 'id' + (new Date()).getTime();
+        marker.addTo(map);
+        marker.addTo(myMarkerGroup);
+        marker.bindPopup("<b>Bod na mapě</b><br>"+uniqId)
+
+        marker.id = uniqId;
+        marker.name = "Název";
+        marker.desctiption = "Popis";
+        marker.latlng = e.latlng;
+        marker.type = "stop";
+
+        var point = {
+            id: uniqId,
+            name: "Název",
+            desctiption: "Popis",
+            latlng: e.latlng,
+            type: "stop"
+        };
+
+        mapPoints.push(point)
+    }
 }
+
+function markersClick(event) {
+    console.log("Marker ID " + event.layer.id);
+    console.log("Marker Name" + event.layer.name);
+    console.log("Marker Desc " + event.layer.desctiption);
+    console.log("Marker LL" + event.layer.latlng);
+    console.log("Marker Type " + event.layer.type);
+
+    $("#name").val(event.layer.name)
+    $("#description").val(event.layer.desctiption)
+    $("#type").val(event.layer.type)
+}
+
+map.on('click', onMapClick);
