3f450dd6f191dc7d7ae7368f92ffbb00ef00ba90
janisska <72078351+janisska@users.noreply.github.com>
2022-01-25 14:03:59 +0100

    wrong file

--- www/stoj10/sp2/javascript/main.js
+++
@@ -1,5 +0,0 @@
-const API_KEY = 'api_key=ccc8923f4778b647253c7f0f5e0f2c6d';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
