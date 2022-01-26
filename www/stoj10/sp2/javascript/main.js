af00c526e4636fe39d45a09066635e44cc8d1444
janisska <72078351+janisska@users.noreply.github.com>
2022-01-26 23:58:51 +0100

    final version

--- www/stoj10/sp2/javascript/main.js
+++ www/stoj10/sp2/javascript/main.js
@@ -1,195 +1,221 @@
+//uses API The Movie Database
+
+const showSpinner = () => {
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner');
+    spinner.innerText = 'Loading...';
+    const body = document.querySelector('main');
+    body.appendChild(spinner);
+    
+    return spinner;
+};
+
 const API_KEY = 'api_key=ccc8923f4778b647253c7f0f5e0f2c6d';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+const searchURL = BASE_URL + '/search/movie?' + API_KEY;
+
+let selectedGenres = []
 
 const genres = [
     {
-      "id": 28,
-      "name": "Action"
+        "id": 28,
+        "name": "Action"
     },
     {
-      "id": 12,
-      "name": "Adventure"
+        "id": 12,
+        "name": "Adventure"
     },
     {
-      "id": 16,
-      "name": "Animation"
+        "id": 16,
+        "name": "Animation"
     },
     {
-      "id": 35,
-      "name": "Comedy"
+        "id": 35,
+        "name": "Comedy"
     },
     {
-      "id": 80,
-      "name": "Crime"
+        "id": 80,
+        "name": "Crime"
     },
     {
-      "id": 99,
-      "name": "Documentary"
+        "id": 99,
+        "name": "Documentary"
     },
     {
-      "id": 18,
-      "name": "Drama"
+        "id": 18,
+        "name": "Drama"
     },
     {
-      "id": 10751,
-      "name": "Family"
+        "id": 10751,
+        "name": "Family"
     },
     {
-      "id": 14,
-      "name": "Fantasy"
+        "id": 14,
+        "name": "Fantasy"
     },
     {
-      "id": 36,
-      "name": "History"
+        "id": 36,
+        "name": "History"
     },
     {
-      "id": 27,
-      "name": "Horror"
+        "id": 27,
+        "name": "Horror"
     },
     {
-      "id": 10402,
-      "name": "Music"
+        "id": 10402,
+        "name": "Music"
     },
     {
-      "id": 9648,
-      "name": "Mystery"
+        "id": 9648,
+        "name": "Mystery"
     },
     {
-      "id": 10749,
-      "name": "Romance"
+        "id": 10749,
+        "name": "Romance"
     },
     {
-      "id": 878,
-      "name": "Science Fiction"
+        "id": 878,
+        "name": "Science Fiction"
     },
     {
-      "id": 53,
-      "name": "Thriller"
+        "id": 53,
+        "name": "Thriller"
     },
     {
-      "id": 10752,
-      "name": "War"
+        "id": 10752,
+        "name": "War"
     }
-  ]
+]
 
-const main = document.getElementById('main');
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-const tagsEl = document.getElementById('tags');
+//creates genres tabs, which can be used as filters, uses the highlightSelection function
+function setGenre(tagsEl, main) {
+    tagsEl.innerHTML = '';
 
-//genres
-var selectedGenre = []
-setGenre();
-function setGenre() {
-    tagsEl.innerHTML='';
     genres.forEach(genre => {
-        const t = document.createElement('div');
-        t.classList.add('tag');
-        t.id=genre.id;
-        t.innerText = genre.name;
-        t.addEventListener('click', () => {
-            if(selectedGenre.length == 0){
-                selectedGenre.push(genre.id);
-            }else{
-                if(selectedGenre.includes(genre.id)){
-                    selectedGenre.forEach((id, idx) => {
-                        if(id == genre.id){
-                            selectedGenre.splice(idx, 1);
-                        }
-                    })
-                }else{
-                    selectedGenre.push(genre.id);
+        const tag = document.createElement('div');
+        tag.classList.add('tag');
+        tag.id = genre.id;
+        tag.innerText = genre.name;
+
+        tag.addEventListener('click', () => {
+            if (selectedGenres.length === 0) {
+                selectedGenres.push(genre.id);
+            } else {
+                if (selectedGenres.includes(genre.id)) {
+                    selectedGenres = selectedGenres.filter((id) => id !== genre.id)
+                } else {
+                    selectedGenres.push(genre.id);
                 }
             }
-            getMovies(API_URL + '&with_genres='+encodeURI(selectedGenre.join(',')))
-            highlightSelection()
+            getMovies(API_URL + '&with_genres=' + encodeURI(selectedGenres.join(',')), main)
+            highlightSelection(tagsEl, main)
         })
-        tagsEl.append(t);
+        tagsEl.append(tag);
     })
 }
 
-function highlightSelection() {
-    const tags = document.querySelectorAll('.tag');
-    tags.forEach(tag => {
-      tag.classList.remove('highlight')
+//highlighting the tabs, uses the clearBtn function
+function highlightSelection(tagsEl, main) {
+    //second click will remove the highlight
+    $('.tag').each(function () {
+        $(this).removeClass('highlight');
     })
-    clearBtn()
-    if (selectedGenre.length !=0){
-        selectedGenre.forEach(id => {
-            const highlightedTag = document.getElementById(id);
-            highlightedTag.classList.add('highlight');
+    clearBtn(tagsEl, main)
+    //first click highlights the tabs
+    if ($(selectedGenres).length > 0) {
+        $.each(selectedGenres, function (index, id) {
+            const highlightedTag = $('#' + id);
+            highlightedTag.addClass('highlight')[0]
         })
     }
 }
 
-function clearBtn(){
+//once genres are selected, a clear button appers and it can reset the selection
+function clearBtn(tagsEl, main) {
     let clearBtn = document.getElementById('clear');
-    if(clearBtn){
+    if (clearBtn) {
         clearBtn.classList.add('highlight')
-    }else{
+    } else {
         let clear = document.createElement('div');
         clear.classList.add('tag', 'highlight');
         clear.id = 'clear';
         clear.innerText = 'Clear x';
         clear.addEventListener('click', () => {
-            selectedGenre = [];
-            setGenre();
-            getMovies(API_URL);
+            selectedGenres = [];
+            setGenre(tagsEl, main);
+            getMovies(API_URL, main);
         })
         tagsEl.append(clear);
     }
 }
 
-//movie cards
-getMovies(API_URL);
-
-function getMovies(url) {
-
-    fetch(url).then(res => res.json()).then(data => {
-        if(data.results.length !== 0){
-            showMovies(data.results);
-        }else{
-            main.innerHTML=`<h2 class="no-results">No Results Found</h2>`
-        }
-    })
-}
-
-function showMovies(data) {
+//creates the movie tabs in the structure with the title, poster and overview
+function showMovies(data, main) {
     main.innerHTML = '';
 
     data.forEach(movie => {
-        const {title, poster_path, overview} = movie;
+        const { title, poster_path, overview } = movie;
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
-                <img src="${IMG_URL+poster_path}" alt="${title}">
-            
-                <div class="movie-info">
-                    <h3>${title}</h3>
-                </div>
-                <div class="overview">
-                    <h3>Overview</h3>
-                    ${overview}
-                </div>
-                `
-        main.appendChild(movieEl);
+            <img src="${IMG_URL + poster_path}" alt="${title}">
         
+            <div class="movie-info">
+                <h3>${title}</h3>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+            `
+        main.appendChild(movieEl);
+
     })
 }
 
-//search field
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const searchTerm = search.value;
-    selectedGenre=[];
-    highlightSelection()
-    if(searchTerm) {
-        getMovies(searchURL+'&query='+searchTerm)
-    }else{
-        getMovies(API_URL);
+//gets the data from TMDB and uses the showMovies function
+async function getMovies(url, main) {
+    const spinner = showSpinner();
+    try {
+        const data = await fetch(url);
+        const jsondata = await data.json();
+        spinner.remove();
+        if (jsondata.results.length !== 0) {
+            showMovies(jsondata.results, main);
+        } else {
+            main.innerHTML = `<h2 class="no-results">No Results Found</h2>`;
+        }
+    } catch (e) {
+        main.innerHTML = `<h2 class="no-results">Developer's mistake</h2>`;
     }
-})
+}
+
+window.onload = () => {
+    const main = $('#main')[0];
+    const search = $('#search')[0];
+    const tagsEl = $('#tags')[0];
+    const form = $('#form')[0];
+
+    //creates genres tabs, which can be used as filters, uses the highlightSelection function
+    setGenre(tagsEl, main);
+
+    //gets the data from TMDB and uses the showMovies function
+    getMovies(API_URL, main);
+
+    //after submiting the form it searches the term and then viewing the results with getMovies function
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const searchTerm = search.value;
+        selectedGenres = [];
+
+        highlightSelection(tagsEl, main)
+        if (searchTerm) {
+            getMovies(searchURL + '&query=' + searchTerm, main)
+        } else {
+            getMovies(API_URL, main);
+        }
+    })
+}
