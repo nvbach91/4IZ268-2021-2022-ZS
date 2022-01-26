f56c4976dac739f9f612a46b1fab7bbd91cf381a
vesl05 <79693886+vesl05@users.noreply.github.com>
2022-01-26 17:39:08 +0100

    version 1.0
    add working filters
    visuals overview
    code formation

--- www/vesl05/sp2/js/app.js
+++ www/vesl05/sp2/js/app.js
@@ -1,26 +1,24 @@
-const map = L.map('map').setView([50.080, 14.451], 15);
+const map = L.map("map").setView([50.080, 14.451], 15);
 
 var currentlyEditedMarker;
 var myMarkerGroup = L.featureGroup().addTo(map).on("click", markersClick);
 var mapPoints = []
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: "mapbox/streets-v11",
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibHVrYW5lIiwiYSI6ImNreGo3cXp6dDZiaTMyeGxhZXkyaGltcnIifQ.uLQYOB-Oo_szmnDn8WwnCw'
+    accessToken: "pk.eyJ1IjoibHVrYW5lIiwiYSI6ImNreGo3cXp6dDZiaTMyeGxhZXkyaGltcnIifQ.uLQYOB-Oo_szmnDn8WwnCw"
 }).addTo(map);
 
 function onMapClick(e) {
 
     var marker = L.marker(e.latlng)
 
-    if($("#add").is(":checked"))
-    {
-        var uniqId = 'id' + (new Date()).getTime();
-        //marker.addTo(map);
+    if ($("#add").is(":checked")) {
+        var uniqId = "id" + (new Date()).getTime();
         marker.addTo(myMarkerGroup);
 
         marker.id = uniqId;
@@ -38,13 +36,12 @@ function onMapClick(e) {
         };
 
         marker.bindPopup("<b>Název</b><br>Popis")
-
         mapPoints.push(point)
     }
 }
 
 function markersClick(event) {
-    currentlyEditedMarker = event.layer.id; 
+    currentlyEditedMarker = event.layer.id;
     $("#name").val(event.layer.name)
     $("#description").val(event.layer.description)
     $("#type").val(event.layer.type)
@@ -52,64 +49,101 @@ function markersClick(event) {
 
 function markerEdit() {
     for (let index in myMarkerGroup._layers) {
-        if(myMarkerGroup._layers[index].id == currentlyEditedMarker){
+        if (myMarkerGroup._layers[index].id == currentlyEditedMarker) {
             myMarkerGroup._layers[index].name = $("#name").val();
             myMarkerGroup._layers[index].description = $("#description").val();
             myMarkerGroup._layers[index].type = $("#type").val();
 
-            myMarkerGroup._layers[index].bindPopup("<b>"+$("#name").val()+"</b><br>" + $("#description").val())
+            myMarkerGroup._layers[index].bindPopup("<b>" + $("#name").val() + "</b><br>" + $("#description").val())
+
+            for (let pointIndex in mapPoints) {
+                if (mapPoints[pointIndex].id == currentlyEditedMarker) {
+                    mapPoints[pointIndex].name = $("#name").val();
+                    mapPoints[pointIndex].description = $("#description").val();
+                    mapPoints[pointIndex].type = $("#type").val();
+                }
+            }
+
         }
     }
 }
 
 function findMyLocation() {
     function success(position) {
-        const latitude  = position.coords.latitude;
+        const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-    
-        map.setView([latitude, longitude], 15);
-      }
 
-    navigator.geolocation.getCurrentPosition(success, function(){});
+        map.setView([latitude, longitude], 15);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, function () { });
 }
 
-function createFile(){
-    console.log("test")
-
+function createFile() {
     var points = [];
 
     for (let index in myMarkerGroup._layers) {
-            points.push({
-                id: myMarkerGroup._layers[index].id,
-                name: myMarkerGroup._layers[index].name,
-                description: myMarkerGroup._layers[index].description,
-                latlng: myMarkerGroup._layers[index].latlng,
-                type:  myMarkerGroup._layers[index].type
-            })
+        points.push({
+            id: myMarkerGroup._layers[index].id,
+            name: myMarkerGroup._layers[index].name,
+            description: myMarkerGroup._layers[index].description,
+            latlng: myMarkerGroup._layers[index].latlng,
+            type: myMarkerGroup._layers[index].type
+        })
     }
 
-    var file = new File([JSON.stringify(points)], "config.json", {type: "application/json"})
+    var file = new File([JSON.stringify(points)], "config.json", { type: "application/json" })
 
     sendFile(file)
 }
 
-function getData(data){
-    for (let index in myMarkerGroup._layers) {
-        map.removeLayer(myMarkerGroup._layers[index])
-    }
+function getData(data) {
+    removeAllPoins()
 
     for (let index = 0; index < data.length; index++) {
-            var marker = L.marker(data[index].latlng)
-            marker.addTo(myMarkerGroup);
+        var marker = L.marker(data[index].latlng)
+        marker.addTo(myMarkerGroup);
 
-            marker.id = data[index].id;
-            marker.name = data[index].name;
-            marker.description = data[index].description;
-            marker.latlng = data[index].latlng;
-            marker.type = data[index].type;
+        marker.id = data[index].id;
+        marker.name = data[index].name;
+        marker.description = data[index].description;
+        marker.latlng = data[index].latlng;
+        marker.type = data[index].type;
 
-            marker.bindPopup("<b>"+data[index].name+"</b><br>" +data[index].description)
+        marker.bindPopup("<b>" + data[index].name + "</b><br>" + data[index].description)
     }
 }
 
-map.on('click', onMapClick);
+function removeAllPoins() {
+    for (let index in myMarkerGroup._layers) {
+        map.removeLayer(myMarkerGroup._layers[index])
+    }
+}
+
+$("#filter").change(function () {
+    removeAllPoins()
+
+    for (let index in mapPoints) {
+        if (mapPoints[index].type == $("#filter").val() || $("#filter").val() == "all") {
+            var marker = L.marker(mapPoints[index].latlng)
+            marker.addTo(myMarkerGroup);
+
+            marker.id = mapPoints[index].id;
+            marker.name = mapPoints[index].name;
+            marker.description = mapPoints[index].description;
+            marker.latlng = mapPoints[index].latlng;
+            marker.type = mapPoints[index].type;
+
+            marker.bindPopup("<b>" + mapPoints[index].name + "</b><br>" + mapPoints[index].description)
+        }
+    }
+});
+
+$(document).keyup(function (e) {
+    if (e.key === "Escape") {
+        map.setView([50.080, 14.451], 15);
+    }
+});
+
+
+map.on("click", onMapClick);
--- www/vesl05/sp2/js/googleAPI.js
+++ www/vesl05/sp2/js/googleAPI.js
@@ -4,15 +4,15 @@
 var GoogleAuth;
 
 function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
+    gapi.load("client:auth2", initClient);
 }
 
 function initClient() {
     gapi.client.init({
-        'apiKey': 'AIzaSyAjrLEcaHIMPgr5aYFzDIWehNH-aVq0_8A',
-        'clientId': '234794660074-a4r8imrqg1emk3selhn0afp4kcss2ud3.apps.googleusercontent.com',
-        'scope': 'https://www.googleapis.com/auth/drive'
-    }).then(function() {
+        "apiKey": "AIzaSyAjrLEcaHIMPgr5aYFzDIWehNH-aVq0_8A",
+        "clientId": "234794660074-a4r8imrqg1emk3selhn0afp4kcss2ud3.apps.googleusercontent.com",
+        "scope": "https://www.googleapis.com/auth/drive"
+    }).then(function () {
         GoogleAuth = gapi.auth2.getAuthInstance();
 
         gapi.client.load("https://content.googleapis.com/discovery/v1/apis/drive/v3/rest")
@@ -22,10 +22,10 @@ function initClient() {
         var user = GoogleAuth.currentUser.get();
         setSigninStatus();
 
-        $('#sign-in-or-out-button').click(function() {
+        $("#sign-in-or-out-button").click(function () {
             handleAuthClick();
         });
-        $('#revoke-access-button').click(function() {
+        $("#revoke-access-button").click(function () {
             revokeAccess();
         });
     });
@@ -49,46 +49,51 @@ function revokeAccess() {
 
 function setSigninStatus() {
     let user = GoogleAuth.currentUser.get();
-    let isAuthorized = user.hasGrantedScopes('https://www.googleapis.com/auth/drive');
+    let isAuthorized = user.hasGrantedScopes("https://www.googleapis.com/auth/drive");
 }
 
 function updateSigninStatus() {
     setSigninStatus();
 }
 
-function sendFile(fileToUpload){
+
+
+//Methods for sending and getting config file
+
+function sendFile(fileToUpload) {
 
     var metadata = {
-    name: fileToUpload.name,
-    mimeType: fileToUpload.type
+        name: fileToUpload.name,
+        mimeType: fileToUpload.type
     };
 
     var formData = new FormData();
-    formData.append( "metadata", new Blob( [JSON.stringify( metadata )], {type: "application/json"} ));
-    formData.append( "file", fileToUpload );
+    formData.append("metadata", new Blob([JSON.stringify(metadata)], { type: "application/json" }));
+    formData.append("file", fileToUpload);
 
-    fetch( "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart", {
-    method: "POST",
-    headers: new Headers({ "Authorization": "Bearer " + gapi.auth.getToken().access_token }),
-    body: formData
-    }).then( function( response ){
-    return response.json();
-    }).then( function( value ){
-    console.log( value );
+    fetch("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart", {
+        method: "POST",
+        headers: new Headers({ "Authorization": "Bearer " + gapi.auth.getToken().access_token }),
+        body: formData
+    }).then(function (response) {
+        return response.json();
+    }).then(function (value) {
+        console.log(value);
     });
 }
 
-function getFile(){
+function getFile() {
     gapi.client.drive.files.list({
         "q": "name='config.json'",
     }).execute(res => {
 
         gapi.client.drive.files.get({
-            "fileId":res.result.files[0].id,
-            "alt":"media"
+            "fileId": res.result.files[0].id,
+            "alt": "media"
         }).execute(res => {
 
             getData(res)
+            
         });
     });
 }
