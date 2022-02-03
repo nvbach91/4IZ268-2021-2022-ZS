f18ae935c9cb9ac7db37a21a127826bbadf4f634
Kruml3 <68549311+Kruml3@users.noreply.github.com>
2022-02-03 07:35:52 +0100

    import logo

--- www/krus02/sp2/src/components/header/AppLogo.js
+++ www/krus02/sp2/src/components/header/AppLogo.js
@@ -1,6 +1,8 @@
+import logo from "../assets/logo.png"
 const AppLogo = ({ children }) => {
   return (
     <div className="flex">
+      <img className="px-6 py-3" src={logo} alt="application logo"/>
       <div className="text-8xl bg-purple-700 p-3 rounded-md text-white">
         SP2
       </div>
