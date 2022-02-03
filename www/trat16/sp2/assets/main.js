382925ed168cc39bbcbec3f80d3548f9693cd78e
Tien Tran <91274666+mrtomis@users.noreply.github.com>
2022-02-03 03:06:13 +0100

    beta +
    - opraveny připomínky
    - přidán spinner pro indikaci načítání
    - přidána chybová hláška pro chybu v načítání informací o počasí
    - přidána možnost vlastního vstupu
    to-do
    - komentáře

--- www/trat16/sp2/assets/main.js
+++ www/trat16/sp2/assets/main.js
@@ -1,48 +1,51 @@
-let cache = {
-    emptyName: false,
-}
-$(document).ready(() => {
-    let weather = {
-        apiKey: "1c54893d31e3e717c91e0ab4a132f9ef",
-        fetchWeather: function (lat, lon, units) {
+$(document)
+    .ready(function () {
+        const overview = $(".weather");
+        const temperature = $(".current-temperature");
+        const wind = $(".wind");
+        const perceived = $(".perceived");
+        const celsius = $(".celsius");
+        const fahrenheit = $(".fahrenheit");
+        const conditions = $(".conditions");
+        const percentage = $(".humidity");
+        const search = $(".search-bar");
+        let cache = {
+            emptyName: false,
+
+        }
+        const apiKey = '1c54893d31e3e717c91e0ab4a132f9ef';
+        function fetchWeather(lat, lon, units) {
             localStorage.setItem('lat', lat);
             localStorage.setItem('lon', lon);
             localStorage.setItem('units', units);
-            $.getJSON(
-                "http://api.openweathermap.org/data/2.5/weather?lat="
-                + lat +
-                "&lon="
-                + lon +
-                "&units="
-                + units +
-                "&lang=cz" +
-                "&appid=" +
-                this.apiKey
-            ).done((data) => {
-                this.showWeather(data);
+            $.getJSON(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&lang=cz&appid=${apiKey}`
+            ).fail(function () {
+                alert('Chyba při načítání dat.');
+            }).done((data) => {
+                showWeather(data);
             });
-        },
-        showWeather: function (data) {
-            $(".weather").slideDown(1000);
+        }
+        function showWeather(data) {
+            overview.slideDown(1000);
             const { icon, description, main } = data.weather[0];
             const { feels_like, temp, humidity } = data.main;
             const { speed } = data.wind;
             const { name } = data;
-            $(".current-temperature").text(Math.round(temp));
+            temperature.text(`${Math.round(temp)}°`);
             $(".icon").attr({
-                src: "http://openweathermap.org/img/wn/" + icon + ".png",
+                src: `http://openweathermap.org/img/wn/${icon}.png`,
                 alt: main
             });
-            if (localStorage.getItem('units') === "metric") {
-                $(".perceived").text("pocitově: " + feels_like + " °C");
-                $(".wind").text("rychlost větru: " + speed + " km/h");
-                $(".celsius").css("color", "black");
-                $(".fahrenheit").css("color", "grey");
+            if (localStorage.getItem('units') === 'metric') {
+                perceived.text(`pocitově: ${feels_like} °C`);
+                wind.text(`rychlost větru: ${speed} km/h`);
+                celsius.css('color', 'black');
+                fahrenheit.css('color', 'grey');
             } else {
-                $(".perceived").text("pocitově: " + feels_like + " °F");
-                $(".wind").text("rychlost větru: " + speed + " mph");
-                $(".celsius").css("color", "grey");
-                $(".fahrenheit").css("color", "black");
+                perceived.text(`pocitově: ${feels_like} °F`);
+                wind.text(`rychlost větru: ${speed} mph`);
+                celsius.css('color', 'grey');
+                fahrenheit.css('color', 'black');
             }
             if (cache.emptyName === true) {
                 const lat = localStorage.getItem('lat');
@@ -51,68 +54,101 @@ $(document).ready(() => {
                 localStorage.setItem('name', name);
                 changeText(name);
             }
-            $(".conditions").text(description);
-            $(".humidity").text("vlhkost: " + humidity + "%");
-        },
-    };
-    $(window).on('popstate', function (event) {
-        if (event.originalEvent.state !== null) {
-            const { lat, lng, name } = event.originalEvent.state;
-            localStorage.setItem('name', name);
-            cache.emptyName = false;
-            changeText(name);
-            weather.fetchWeather(lat, lng, localStorage.getItem('units'));
-        } else {
-            $("title").text("Super Weather");
-            $(".weather").slideUp(1000);
-            $(".search-bar").val('');
+            conditions.text(description);
+            percentage.text(`vlhkost: ${humidity}%`);
         }
-    });
-    function changeText(name) {
-        $("title").text("Super Weather | " + name);
-        $(".location").text("Počasí v " + name);
-    }
-    if (localStorage.getItem('units') === null) {
-        localStorage.setItem('units', "metric");
-    }
-    if (localStorage.getItem('lat') !== null && localStorage.getItem('lon') !== null && localStorage.getItem('name') !== null) {
-        changeText(localStorage.getItem('name'));
-        cache.emptyName = false;
-        weather.fetchWeather(localStorage.getItem('lat'), localStorage.getItem('lon'), localStorage.getItem('units'));
-    }
-    
-    function getUserLocation() {
-        navigator.geolocation.getCurrentPosition(function (data) {
-            $(".search-bar").val('');
-            cache.emptyName = true;
-            weather.fetchWeather(data.coords.latitude, data.coords.longitude, localStorage.getItem('units'));
-        });
-    };
-    function changeUnits(unit) {
-        if (unit === "metric") {
-            weather.fetchWeather(localStorage.getItem('lat'), localStorage.getItem('lon'), "metric");
-        } else {
-            weather.fetchWeather(localStorage.getItem('lat'), localStorage.getItem('lon'), "imperial");
-        }
-    };
 
-    const placesAutocomplete = places({
-        appId: 'plUKYE6MHLZW',
-        apiKey: '494e4aad2936484cc24439027b7a32e6',
-        container: $('.search-bar')[0]
-    });
-    function getLocation(data) {
-        const { lat, lng } = data.latlng;
-        const { name } = data;
-        history.pushState({ lat, lng, name }, null);
-        localStorage.setItem('name', name);
-        changeText(name);
-        cache.emptyName = false;
-        weather.fetchWeather(lat, lng, localStorage.getItem('units'));
-    }
-    placesAutocomplete.on('change', e => getLocation(e.suggestion));
-    $(".fahrenheit").click(() => { changeUnits("imperial"); });
-    $(".celsius").click(() => { changeUnits("metric") });
-    $(".position").click(() => { getUserLocation() });
-});
+        $(window).on('popstate', function (event) {
+            if (event.originalEvent.state !== null) {
+                const { lat, lng, name } = event.originalEvent.state;
+                localStorage.setItem('name', name);
+                cache.emptyName = false;
+                changeText(name);
+                fetchWeather(lat, lng, localStorage.getItem('units'));
+            } else {
+                $("title").text('Super Weather');
+                overview.slideUp(1000);
+                search.val('');
+            }
+        });
+        function changeText(name) {
+            $("title").text(`Super Weather | ${name}`);
+            $(".location").text(`Počasí v ${name}`);
+        }
+        if (localStorage.getItem('units') === null) {
+            localStorage.setItem('units', 'metric');
+        }
+        if (localStorage.getItem('lat') !== null && localStorage.getItem('lon') !== null && localStorage.getItem('name') !== null) {
+            changeText(localStorage.getItem('name'));
+            cache.emptyName = false;
+            fetchWeather(localStorage.getItem('lat'), localStorage.getItem('lon'), localStorage.getItem('units'));
+        }
+
+        function getUserLocation() {
+            navigator.geolocation.getCurrentPosition(function (data) {
+                search.val('');
+                cache.emptyName = true;
+                fetchWeather(data.coords.latitude, data.coords.longitude, localStorage.getItem('units'));
+            });
+        };
+        function changeUnits(unit) {
+            if (unit === 'metric') {
+                fetchWeather(localStorage.getItem('lat'), localStorage.getItem('lon'), 'metric');
+            } else {
+                fetchWeather(localStorage.getItem('lat'), localStorage.getItem('lon'), 'imperial');
+            }
+        };
+        function useUserInput(value) {
+            const validCoordinates = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/;
+            if (!value.match(validCoordinates)) {
+                search.val('Zadali jste chybné údaje');
+
+            } else {
+                let inputValue = value.trim();
+                let coordinates = inputValue.split(',');
+                try {
+                    cache.emptyName = true;
+                    fetchWeather(parseFloat(coordinates[0]), parseFloat(coordinates[1]), localStorage.getItem('units'));
+                } catch (error) {
+                    alert('Chyba vstupu');
+                }
+            }
+        }
+
+        const placesAutocomplete = places({
+            appId: 'plUKYE6MHLZW',
+            apiKey: '494e4aad2936484cc24439027b7a32e6',
+            container: search[0]
+        });
+
+        function getLocation(data) {
+            const { lat, lng } = data.latlng;
+            const { name } = data;
+            history.pushState({ lat, lng, name }, null);
+            localStorage.setItem('name', name);
+            changeText(name);
+            cache.emptyName = false;
+            fetchWeather(lat, lng, localStorage.getItem('units'));
+        }
+        placesAutocomplete.on("change", e => getLocation(e.suggestion));
+        
+        search.on("keypress", function (e) {
+            if (e.which == 13) {
+                useUserInput(search.val());
+            }
+        })
+        fahrenheit.click(() => { changeUnits('imperial'); });
+        celsius.click(() => { changeUnits('metric') });
+        $(".position").click(() => { getUserLocation() });
+        $(".search-button").click(() => { useUserInput(search.val()) });
+    })
+    .ajaxStart(function () {
+        $(".loading").show();
+        $(".info").hide();
+    })
+    .ajaxStop(function () {
+        $(".loading").hide();
+        $(".info").show()
+    })
+
 
