630a2ef0e84e9a4c663d3a265cce634a998e125b
maka090 <91321416+maka090@users.noreply.github.com>
2022-02-03 13:25:15 +0100

    sp2 oprava

--- www/maka09/sp2/JS/script.js
+++ www/maka09/sp2/JS/script.js
@@ -1,18 +1,21 @@
-const form = document.querySelector('form');
-const factDiv = document.querySelector('.number-fact');
-const getRndm = document.querySelector('.getRandom');
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-function randomInteger(min, max) {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  }
+   const form = document.querySelector('form');
+   const factDiv = document.querySelector('.number-fact');
+   const getRndm = document.querySelector('.getRandom');
 
-function randomType(){
-   const num =  randomInteger(1,4);
-   if (num == 1) {
-      const typ = "trivia";
-      console.log(typ);
-      return typ;
+//funkce na generování náhodného čísla 
+   function randomInteger(min, max) {
+      let rand = min + Math.random() * (max + 1 - min);
+      return Math.floor(rand);
+   }
+//funkce na zvolení typu faktu pomocí zgenerovaného čísla 
+   function randomType(){
+      const num =  randomInteger(1,4);
+      if (num == 1) {
+         const typ = "trivia";
+         console.log(typ);
+         return typ;
    } 
 
    if (num == 2){
@@ -31,41 +34,44 @@ function randomType(){
     const typ = "date";
     console.log(typ);
     return typ;
- }
+   }
    
    
-}
+   }
 
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const loadText ="wait a little bit ⌛";
+  const loadText ='wait a little bit ⌛';
   factDiv.innerHTML = loadText;
-  const baseURL = "http://numbersapi.com/";
+  const baseURL = 'https://cors-anywhere.herokuapp.com/http://numbersapi.com/';
   const number = e.target.querySelector('input[type="number"]').value;
   fetch(baseURL + number,{
     headers:{
       'x-requested-with': 'text/plain'
     }})
     .then(response => response.text())
-    .then(text => factDiv.innerHTML = text)
+    .then(text => factDiv.innerText = text)
     .catch(e=>console.log(e));
 })
 
+//Generování faktu o náhodnem čísle
 getRndm.addEventListener('click', () => {
-    const loadText ="wait a little bit ⌛";
+    const loadText ='wait a little bit ⌛';
     factDiv.innerHTML = loadText;
-    const baseURL = "http://numbersapi.com/";
+    const baseURL = 'https://cors-anywhere.herokuapp.com/http://numbersapi.com/';
     const number = randomInteger(0, 2000);
-    const typ = "/" + randomType();
+    console.log(number);
+    const typ = '/' + randomType();
     fetch(baseURL + number + typ,{
         headers:{
             'x-requested-with': 'text/plain'
           }})
           .then(response => response.text())
-          .then(text => factDiv.innerHTML = text)
+          .then(text => factDiv.innerText = text)
           .catch(e=>console.log(e));
 
     })
 
 
+ });
