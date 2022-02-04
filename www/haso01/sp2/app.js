ad2fb22b95d56b064faf9a624937275b8b2e595e
haso01 <92163937+haso01@users.noreply.github.com>
2022-02-04 13:03:08 +0100

    version 02
    charged most of stuff

--- www/haso01/sp2/app.js
+++ www/haso01/sp2/app.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
 	const weatherForm = $('#weather-input-form');
-    const cityNameInput = $('input[name="city-name"]');
+	const cityNameInput = $('input[name="city-name"]');
 	const apiKey = '&appid=49e168c62ed9de1a220b5ebdbeca7b56';
 	let daily = [];
 	let cityName = "";
@@ -16,10 +16,42 @@ $(document).ready(() => {
 	mapIcon.set('13', "images/icons/icon-14.svg");
 	mapIcon.set('50', "images/icons/icon-8.svg");
 
+
+	/*fetch('https://geolocation-db.com/json/').then((resp) => {
+		return resp.json();	
+	}).then((resp) => {
+		const cityValue = {
+			city: resp.city
+		};
+		console.log(cityValue.city);
+		cityNameInput.val(cityValue.city);
+	})*/
+
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(getPosition);
+	} else {
+		x.innerHTML = "Geolocation is not supported by this browser.";
+	}
+
+	function getPosition(position) {
+		var lat = position.coords.latitude;
+		var long = position.coords.longitude;
+		fetch('http://api.openweathermap.org/geo/1.0/reverse?lat=' + lat + '&lon=' + long + '&limit=5&appid=49e168c62ed9de1a220b5ebdbeca7b56').then((resp) => {
+			return resp.json();
+		}).then((resp) => {
+			const cityValue = {
+				name: resp[0].name
+			};
+			console.log(cityValue);
+			cityNameInput.val(cityValue.name);
+			weatherForm.submit();
+		})
+	}
+
 	weatherForm.submit((event) => {
 		event.preventDefault();
 		const inputValue = cityNameInput.val();
-		if(inputValue.trim() == ""){
+		if (inputValue.trim() == "") {
 			return;
 		}
 		fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + inputValue + apiKey).then((resp) => {
@@ -31,14 +63,13 @@ $(document).ready(() => {
 				lon: resp[0].lon
 			};
 			cityName = city.name;
-			console.log(city.name);
 			fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + city.lat + '&lon=' + city.lon + '&exclude=minutely,hourly,alerts&units=metric' + apiKey).then((resp) => {
 				return resp.json();
 			}).then((resp) => {
 				const locale = "en-us";
 				const currentWeather = resp.current;
-				var date = new Date(currentWeather.dt*1000);
-				const currentData =  {
+				var date = new Date(currentWeather.dt * 1000);
+				const currentData = {
 					dt: date.toLocaleDateString(locale, { weekday: 'long' }),
 					temp: Math.round(currentWeather.temp),
 					weatherMain: currentWeather.weather[0].main,
@@ -47,23 +78,22 @@ $(document).ready(() => {
 					wind_speed: currentWeather.wind_speed
 				}
 				const weatherDaily = resp.daily;
-				$.each(weatherDaily, function(i,jsonObject){
-					var date = new Date(weatherDaily[i].dt*1000); 	
-						const weatherData = {
-							dt: date.toLocaleDateString(locale, { weekday: 'long' }),
-							tempDay: Math.round(weatherDaily[i].temp.day),
-							tempNight: Math.round(weatherDaily[i].temp.night),
-							weatherMain: weatherDaily[i].weather[0].main,
-							weatherIcon: mapIcon.get(weatherDaily[i].weather[0].icon.substring(0, 2)),
-							humidity: weatherDaily[i].humidity,
-							wind_speed: weatherDaily[i].wind_speed
-						}
-						daily.push(weatherData);
-					});
-					daily.pop();
-					daily[0] = currentData;
-					
-					console.log(daily);
+				$.each(weatherDaily, function (i, jsonObject) {
+					var date = new Date(weatherDaily[i].dt * 1000);
+					const weatherData = {
+						dt: date.toLocaleDateString(locale, { weekday: 'long' }),
+						tempDay: Math.round(weatherDaily[i].temp.day),
+						tempNight: Math.round(weatherDaily[i].temp.night),
+						weatherMain: weatherDaily[i].weather[0].main,
+						weatherIcon: mapIcon.get(weatherDaily[i].weather[0].icon.substring(0, 2)),
+						humidity: weatherDaily[i].humidity,
+						wind_speed: weatherDaily[i].wind_speed
+					}
+					daily.push(weatherData);
+				});
+				daily.pop();
+				daily[0] = currentData;
+
 			}).then(() => {
 				const dailyWeatherStats = $('#daily-forecast');
 				const dailyWeatherContent = dailyWeatherStats.find('.forecast');
@@ -88,9 +118,9 @@ $(document).ready(() => {
 				);
 				dailyWeatherStats.append(weatherContainer);
 				daily.shift();
-				console.log(daily);
+				const dailyStats = [];
 
-				$.each(daily, function(i,jsonObject){ 	
+				$.each(daily, function (i, jsonObject) {
 					const dailyWeatherContainer = $(`
 					<div class="forecast">
 						<div class="forecast-header">
@@ -98,21 +128,23 @@ $(document).ready(() => {
 						</div> <!-- .forecast-header -->
 						<div class="forecast-content">
 							<div class="forecast-icon">
-								<img src="` + daily[i].weatherIcon + `" alt="" width=48>
+								<img src="` + daily[i].weatherIcon + `" alt="" width="48">
 							</div>
 							<div class="degree">` + daily[i].tempDay + `<sup>o</sup>C</div>
 							<small>` + daily[i].tempNight + `<sup>o</sup>C</small>
 						</div>
 					</div>`
-					
-				)
-				dailyWeatherStats.append(dailyWeatherContainer);
+
+					)
+					dailyStats.push(dailyWeatherContainer);
+					//dailyWeatherStats.append(dailyWeatherContainer);
 				})
+				dailyWeatherStats.append(dailyStats)
 				daily = [];
 				const mapStats = $('#map-content');
 				const mapContent = mapStats.find('#map');
 				mapContent.remove();
-				
+
 				const mapContainer = $(`
 				<div id="map" class="map">
 					<iframe src="https://openweathermap.org/weathermap?basemap=map&cities=false&layer=radar&lat=` + city.lat + `&lon=` + city.lon + `&zoom=7" width="700" height="500" allowfullscreen="" loading="lazy" class="map"></iframe>
