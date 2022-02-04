add0c06f8e0f9a5cadfe7b243c7e98e5582d4cb9
KoresOndra <91332449+KoresOndra@users.noreply.github.com>
2022-02-04 15:08:08 +0100

    version 03
    added functions during exam

--- www/novo09/sp2/assets/js/main.js
+++ www/novo09/sp2/assets/js/main.js
@@ -83,33 +83,6 @@ App.appendPre = (message) => {
 };
 
 
-/**
- * Print the names and majors of students in a sample spreadsheet:
- * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- */
-//  App.listData = () => {
-//     const tableBody = document.querySelector('#table-body');
-//     const rows = [];
-//     gapi.client.sheets.spreadsheets.values.get({
-//         spreadsheetId: App.spreadsheetId,
-//         range: 'Sheet1!A1:D101',
-//     }).then((response) => {
-//         const range = response.result;
-//         if (range.values.length > 0) {
-//             for (i = 0; i < range.values.length; i++) {
-//                 const row = range.values[i];
-//                 rows.push(row);
-//             }
-//             tableBody.innerHTML = rows.map((row) => `<tr>${row.map((value) => `<td>${value}</td>`).join('')}</tr>`).join('');
-//         } else {
-//             App.appendPre('No data found.');
-//         }
-//     }, (response) => {
-//         App.appendPre('Error: ' + response.result.error.message);
-//     });
-// };
-
-
 
 
 $(document).ready(() => {
@@ -117,49 +90,61 @@ $(document).ready(() => {
     App.authorizeButton = $('#authorize_button');
     App.signoutButton = $('#signout_button');
     App.signoutButton = $('#signout_button');
-    //App.uploadButton = $('#save-to-drive');
-
-    
 
 
-    // App.addForm = $('#add-form').submit((e) => {
-    //     e.preventDefault();
-    //     const data = {};
-    //     App.addForm.serializeArray().forEach(({ name, value }) => data[name] = value);
-    //     gapi.client.sheets.spreadsheets.values.append({
-    //         spreadsheetId: App.spreadsheetId,
-    //         range: 'Sheet1!A1',
-    //         valueInputOption: 'RAW',
-    //         insertDataOption: 'INSERT_ROWS',
-    //         resource: {
-    //             values: [
-    //                 [data.id, data.name, data.birth, data.height]
-    //             ]
-    //         }
-    //     }).then((response) => {
-    //         var result = response.result;
-    //         //console.log(`${result.updates.updatedCells} cells appended.`);
-    //         App.listData();
-    //     });
+    let routesAreNotLoaded = true;
 
-    // });
-
-    const uploadButton = $('#save-to-drive');
-    uploadButton.click(()=>{
-        console.log('klik');
-        if(!gapi.auth2.getAuthInstance().isSignedIn.get()){
+    /**
+    *  Function of button to upload route to google sheet
+    */
+    const uploadButton = $('#save-to-sheet');
+    uploadButton.click(() => {
+        const tripName = $('input[name="new-trip-name"]').val();
+        if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
             alert('Pro uložení je nutné být autentizován');
-        } else if(coords.length<2){
+        } else if (coords.length < 2) {
             alert('Trasa musí obsahovat alespň dva body než je možné ji uložit');
+        } else if (tripName == "") {
+            alert('Je třeba trasu nějak pojmenovat');
         } else {
-            const tripName = $('input[name="new-trip-name"]').val();
-            console.log(tripName + ' name');
+            routesAreNotLoaded=true;
+            loadRouteNames();
+            console.log('name of trip: ' + tripName);
             let routeToSave = [tripName];
-            function addCoordinate(coordinates){
+            function addCoordinate(coordinates) {
                 routeToSave.push(coordinates.toString());
                 console.log(coordinates.toString());
             }
             coords.forEach(addCoordinate);
+
+
+
+            gapi.client.sheets.spreadsheets.values.get({
+                spreadsheetId: App.spreadsheetId,
+                range: 'Sheet1!A1:D101',
+            }).then((response) => {
+                const range = response.result;
+                let rowIndex = 0;
+                if (range.values.length > 0) {
+                    for (i = 0; i < range.values.length; i++) {
+                        const row = range.values[i];
+                        const name = row[0];
+                        if (tripName == name) {
+                            deleteRow(rowIndex);
+                            break;
+                        }
+                        rowIndex++;
+                        console.log(name);
+                    }
+
+                } else {
+                    App.appendPre('No data found.');
+                }
+            }, (response) => {
+                App.appendPre('Error: ' + response.result.error.message);
+            });
+
+
 
             gapi.client.sheets.spreadsheets.values.append({
                 spreadsheetId: App.spreadsheetId,
@@ -173,36 +158,124 @@ $(document).ready(() => {
                 }
             }).then((response) => {
                 var result = response.result;
-                //console.log(`${result.updates.updatedCells} cells appended.`);
             });
+
+
+        }
+    });
+    
+    const searchTripField = $('#search-trip');
+    searchTripField.change(() => {
+        console.log('clicked')
+        if (routesAreNotLoaded) {
+            console.log('ran command')
+            loadRouteNames();
+            routesAreNotLoaded = false;
         }
     });
 
-    function loadCoordinate(coordinate){
+
+    const routeOptionsDatalist = $('#available-trips');
+
+    const loadRouteNames = function () {
+        //const loadedRouteNames = [];
+        const datalistContent = [];
+        gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: App.spreadsheetId,
+            range: 'Sheet1!A1:D101',
+        }).then((response) => {
+            const range = response.result;
+            if (range.values.length > 0) {
+                for (i = 0; i < range.values.length; i++) {
+                    const row = range.values[i];
+                    const name = row[0];
+                    //loadedRouteNames.push(name);
+                    //console.log(name);
+                    const menuLine = '<option value="' + name + '">'+name+'</option>';
+                    datalistContent.push(menuLine);
+                }
+
+            } else {
+                App.appendPre('No data found.');
+            }
+            routeOptionsDatalist.html("");
+            routeOptionsDatalist.append(datalistContent);
+        }, (response) => {
+            App.appendPre('Error: ' + response.result.error.message);
+            
+        });
+
+        
+
+        //console.log(loadedRouteNames);
+        console.log(datalistContent);
+    }
+
+
+
+    // const loadRoutesButton = $('#load-routes');
+    // loadRoutesButton.click(loadRouteNames);
+
+
+    const deleteRow = function (rowId) {
+        console.log('attempted deletion')
+        gapi.client.sheets.spreadsheets.batchUpdate({
+            // auth: auth,
+            spreadsheetId: App.spreadsheetId,
+            resource: {
+                "requests": [
+                    {
+                        "deleteDimension": {
+                            "range": {
+                                "sheetId": 0,
+                                "dimension": "ROWS",
+                                "startIndex": rowId,
+                                "endIndex": rowId + 1
+                            }
+                        }
+                    },
+                ],
+            }
+        }).then((response) => {
+            //var findReplaceResponse = response.result.replies[1].findReplace;
+            //console.log(`${findReplaceResponse.occurrencesChanged} replacements made.`);
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     *  Loading coordinates to coords array
+    */
+    function loadCoordinate(coordinate) {
         const consenantIndex = coordinate.indexOf(",");
         const longtitude = coordinate.slice(1, consenantIndex);
-        const latitude = coordinate.slice(consenantIndex+1, coordinate.length-1);
+        const latitude = coordinate.slice(consenantIndex + 1, coordinate.length - 1);
         coords.push(SMap.Coords.fromWGS84(longtitude, latitude));
     }
 
-    function testFunction(item){
-        const consenantIndex = item.indexOf(",");
-        const longtitude = item.slice(1, consenantIndex);
-        const latitude = item.slice(consenantIndex+1, item.length-1);
-        console.log(item);
-        console.log(longtitude);
-        console.log(latitude);
-    }
 
+    /**
+    *  Function of butto loading requested route from google sheet.
+    */
     const loadRouteButton = $('#load-trip');
-    loadRouteButton.click(()=>{
-        if(!gapi.auth2.getAuthInstance().isSignedIn.get()){
+    loadRouteButton.click(() => {
+        const savedTripName = $('input[id="search-trip"]').val();
+        if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
             alert('Pro načtení trasy je nutné být autentizován');
+        } else if (savedTripName == "") {
+            alert('Zadejte jméno hledané trasy');
         } else {
-            //const tableBody = document.querySelector('#table-body');
-            const savedTripName = $('input[name="search-trip"]').val();
-            
-            //const rows = [];
+            console.log('reading '+ savedTripName);
             gapi.client.sheets.spreadsheets.values.get({
                 spreadsheetId: App.spreadsheetId,
                 range: 'Sheet1!A1:D101',
@@ -211,109 +284,130 @@ $(document).ready(() => {
                 if (range.values.length > 0) {
                     for (i = 0; i < range.values.length; i++) {
                         const row = range.values[i];
-                        if(row[0]==savedTripName){
-                            console.log('found')
+                        if (row[0] == savedTripName) {
+                            console.log('found trip')
                             console.log(row);
                             const loadedRoute = row;
                             loadedRoute.shift();
                             coords.length = 0;
                             vrstva.removeAll();
-                            //loadedRoute.forEach(testFunction)
                             loadedRoute.forEach(loadCoordinate);
-                            redrawRoute();
+                            route = new SMap.Route(coords, redrawAndCenter);
                             break;
                         }
                     }
-                    //tableBody.innerHTML = rows.map((row) => `<tr>${row.map((value) => `<td>${value}</td>`).join('')}</tr>`).join('');
+
                 } else {
                     App.appendPre('No data found.');
                 }
             }, (response) => {
                 App.appendPre('Error: ' + response.result.error.message);
             });
-            //console.log(loadedRoute);
-            // loadedRoute.shift();
-            // console.log(loadedRoute);
-            // coords.length = 0;
-            // loadedRoute.forEach(loadCoordinate);
-            // vrstva.removeAll();
-            // redrawRoute();
         }
     });
 
 
+    //default coordinates of map center
+    let currentLatitude = 50.084;
+    let currentLongtitude = 14.434;
 
 
-    
-    const centerMap = SMap.Coords.fromWGS84(14.40, 50.08);
+    /**
+    *  Centering map and arker on current location
+    */
+
+    const centerOnCurrLocation = function () {
+        navigator.geolocation.getCurrentPosition((position) => {
+            currentLatitude = position.coords.latitude;
+            currentLongtitude = position.coords.longitude;
+            centerMap = SMap.Coords.fromWGS84(currentLongtitude, currentLatitude);
+            m.setCenter(centerMap, true);
+            marker.setCoords(centerMap);
+            console.log(centerMap);
+        });
+    }
+
+    centerOnCurrLocation();
+
+    let centerMap = SMap.Coords.fromWGS84(currentLongtitude, currentLatitude);
     const m = new SMap(JAK.gel("mapa"), centerMap, 10);
     const l = m.addDefaultLayer(SMap.DEF_BASE).enable();
     const mouse = new SMap.Control.Mouse(SMap.MOUSE_PAN | SMap.MOUSE_WHEEL | SMap.MOUSE_ZOOM); /* Ovládání myší */
-    m.addControl(mouse);     
-    
+    m.addControl(mouse);
+
+
+
 
     const markerLayer = new SMap.Layer.Marker();
     m.addLayer(markerLayer).enable();
-    const marker = new SMap.Marker(SMap.Coords.fromWGS84(14.434, 50.084));
+    let marker = new SMap.Marker(centerMap);
     marker.decorate(SMap.Marker.Feature.Draggable);
     markerLayer.addMarker(marker);
 
+
+    // set marker for each point of insterest
+    const placeMarker = function (markerlatitude, markerlongtitude) {
+        const markerLayer = new SMap.Layer.Marker();
+        m.addLayer(markerLayer).enable();
+        const marker = new SMap.Marker(SMap.Coords.fromWGS84(markerlongtitude, markerlatitude));
+        //marker.decorate(SMap.Marker.Feature.Draggable);
+        markerLayer.addMarker(marker);
+    }
+
+
+
     const addPointButton = $('#add-point');
-    addPointButton.click(()=>{
-        
+    addPointButton.click(() => {
+
         markerCoords = marker.getCoords();
-        if(coords[coords.length - 1] != markerCoords){
+        if (coords[coords.length - 1] != markerCoords) {
             coords.push(markerCoords);
             redrawRoute();
-        }else{
+        } else {
             console.log('already contains');
         }
-        // let markerCoords = null;
-        // function stop(e) {
-        //     if(markerCoords != null){
-        //         const index = coords.indexOf(markerCoords);
-        //         coords.splice(index,1);
-        //     }
+    });
 
-        //     const node = e.target.getContainer();
-        //     node[SMap.LAYER_MARKER].style.cursor = "";
-        //     markerCoords = e.target.getCoords();
-        //     coords.push(markerCoords);
-        //     route = new SMap.Route(coords, nalezeno);
-            
-        // }
-        
-        // const signals = m.getSignals();
-        // signals.addListener(window, "marker-drag-stop", stop);    
-   });
+    const centerOnCurrLocationButton = $('#center-on-location');
+    centerOnCurrLocationButton.click(() => {
+        centerOnCurrLocation();
+    });
 
-   const redrawRoute = function() {
-        if(coords.length>1){
-            route = new SMap.Route(coords, nalezeno);
+
+
+    const redrawRoute = function () {
+        if (coords.length > 1) {
+            route = new SMap.Route(coords, found);
+        } else {
+            vrstva.removeAll();
         }
-   }
-   const removeLastPointButton = $('#remove-point');
-   removeLastPointButton.click(()=>{
+    }
+    const removeLastPointButton = $('#remove-point');
+    removeLastPointButton.click(() => {
         coords.pop();
         redrawRoute();
-   });
+    });
 
     const vrstva = new SMap.Layer.Geometry();
     m.addLayer(vrstva).enable();
 
-   const startOverButton = $('#start-over');
-   startOverButton.click(()=>{
+    const startOverButton = $('#start-over');
+    startOverButton.click(() => {
         coords.length = 0;
         vrstva.removeAll();
-   });
+    });
 
 
-   
+    const redrawAndCenter = function (route) {
+        var coords = route.getResults().geometry;
+        var cz = m.computeCenterZoom(coords);
+        m.setCenterZoom(cz[0], cz[1]);
+        var g = new SMap.Geometry(SMap.GEOMETRY_POLYLINE, null, coords);
+        vrstva.addGeometry(g);
+    }
 
-    const nalezeno = function(route) {
+    const found = function (route) {
         const coords = route.getResults().geometry;
-            // const cz = m.computeCenterZoom(coords);
-            // m.setCenterZoom(cz[0], cz[1]);
         const g = new SMap.Geometry(SMap.GEOMETRY_POLYLINE, null, coords);
         vrstva.removeAll();
         vrstva.addGeometry(g);
@@ -321,7 +415,6 @@ $(document).ready(() => {
 
     const coords = [];
     let route;
-
 
 
 });
