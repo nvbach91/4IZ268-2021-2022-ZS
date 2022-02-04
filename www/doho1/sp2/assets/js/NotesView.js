4dceef9fd2442334ebc1996078525792666d29c4
Do Viet Anh <64709541+AirbornBird88@users.noreply.github.com>
2022-02-04 15:12:09 +0100

    version 3.0

---
+++ www/doho1/sp2/assets/js/NotesView.js
@@ -0,0 +1,102 @@
+// rendering view for notes class
+export default class NotesUI {
+    constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete }) {
+        this.root = root;
+        this.onNoteSelect = onNoteSelect;
+        this.onNoteAdd = onNoteAdd;
+        this.onNoteEdit = onNoteEdit;
+        this.onNoteDelete = onNoteDelete;
+        this.root.innerHTML = `
+        <div class="notes-sidebar">
+            <button class="notes-add" type="button">Add note</button>
+            <div class="notes-list"></div>
+        </div>
+        <hr>
+        <div class="notes-wrapper">
+            <input class="notes-title" type="text" placeholder="Enter a title..." spellcheck="false">
+            <textarea class="notes-body" id="notes-body" name="notes-body" rows="6" cols="60"
+                placeholder="Enter your notes here..." required spellcheck="false"></textarea>
+        </div>
+        `;
+
+        const btnAddNote = this.root.querySelector(".notes-add");
+        const inpTitle = this.root.querySelector(".notes-title");
+        const inpBody = this.root.querySelector(".notes-body");
+
+        btnAddNote.addEventListener("click", () => {
+            this.onNoteAdd();
+        });
+
+        [inpTitle, inpBody].forEach(inputField => {
+            inputField.addEventListener("blur", () => {
+                const updatedTitle = inpTitle.value.trim();
+                const updatedBody = inpBody.value.trim();
+
+                this.onNoteEdit(updatedTitle, updatedBody);
+            });
+        });
+
+        this.updateNotePreviewVisibility(false);
+    }
+
+    _createListItemHTML(id, title, body, time, temp) {
+        const MAX_BODY_LENGTH = 60;
+
+        return `
+            <div class="notes-list-item" data-note-id="${id}">
+                <div class="notes-item-title">${title}</div>
+                <div class="notes-item-body">
+                    ${body.substring(0, MAX_BODY_LENGTH)}
+                    ${body.length > MAX_BODY_LENGTH ? "..." : ""}
+                </div>
+                <div class="notes-item-time" data-note-id="${id}">
+                    ${time.toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}
+                </div>
+            </div>
+        `;
+    }
+
+    updateNoteList(notes) {
+        const notesListContainer = this.root.querySelector(".notes-list");
+
+        // Empty list
+        notesListContainer.innerHTML = "";
+        let tempHtml = [];
+        for (const note of notes) {
+            const html = this._createListItemHTML(note.id, note.title, note.body, new Date(note.time), note.temp);
+            tempHtml.push(html);
+        }
+        notesListContainer.insertAdjacentHTML("beforeend", tempHtml.join(''));
+
+
+        // Add select/delete events for each list item
+        notesListContainer.querySelectorAll(".notes-list-item").forEach(noteListItem => {
+            noteListItem.addEventListener("click", () => {
+                this.onNoteSelect(noteListItem.dataset.noteId);
+            });
+
+            noteListItem.addEventListener("dblclick", () => {
+                const doDelete = confirm("Are you sure you want to delete this note?");
+
+                if (doDelete) {
+                    this.onNoteDelete(noteListItem.dataset.noteId);
+                }
+            });
+        });
+    }
+
+    updateActiveNote(note) {
+        this.root.querySelector(".notes-title").value = note.title;
+        this.root.querySelector(".notes-body").value = note.body;
+
+        this.root.querySelectorAll(".notes-list-item").forEach(noteListItem => {
+            noteListItem.classList.remove("notes-list-item-selected");
+        });
+
+        this.root.querySelector(`.notes-list-item[data-note-id="${note.id}"]`).classList.add("notes-list-item-selected");
+    }
+
+    updateNotePreviewVisibility(visible) {
+        this.root.querySelector(".notes-wrapper").style.visibility = visible ? "inherit" : "hidden";
+    }
+}
---
+++ www/doho1/sp2/assets/js/map.js
@@ -0,0 +1,319 @@
+//initialize map
+function initMap() {
+
+    let myLatlng = { lat: 60.073658, lng: 14.418540 };
+    let markers = [];
+    let notes = document.querySelector('#notes');
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: myLatlng,
+        zoom: 8,
+    });
+
+    //asking for geolocation API
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+        myLatlng = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        map.setCenter(myLatlng);
+        });
+    }
+
+
+    //add custom marker
+    const svgMarker = {
+        path: "M2 15.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v13.5zM8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z",
+        fillColor: "blue",
+        fillOpacity: 0.5,
+        strokeWeight: 0,
+        rotation: 0,
+        scale: 2,
+        anchor: new google.maps.Point(6, 6),
+    };
+    
+    // show all markers from local storage (created in previous sessions) map 
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = JSON.parse(localStorage.key(i));
+        
+        const marker = new google.maps.Marker({
+            position: new google.maps.LatLng(key.lat, key.lng),
+            map,
+            title: "Click to zoom, double click to delete",
+            icon: svgMarker,
+            label: `${i + 1}`,
+            animation: google.maps.Animation.DROP,
+        });
+        const noteWindow = new google.maps.InfoWindow({
+            content: "Please wait few moments, I am retrieving info..."
+        });
+
+        markers.push(marker);
+
+        // after 2s get adress of another marker
+        //setTimeout was set up to prevent sending too much request to Google in a small amount of time
+        window.setTimeout(() => {
+            geocodeLatLng(map, noteWindow, marker);
+        }, i * 2000);
+
+
+        marker.addListener("click", () => {
+            notes.style.visibility = "visible";
+            sessionStorage.setItem('helper', JSON.stringify(marker.position.toJSON()));
+
+            //search for the already bouncing marker to then stop this animation and "asign" it to clicked marker
+            const bouncing = markers.find(marker => marker.animation == google.maps.Animation.BOUNCE);
+            if (bouncing) {
+                bouncing.setAnimation(null);
+            }
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+
+            //after click it also opens a note window
+            noteWindow.open({
+                anchor: marker,
+                shouldFocus: true,
+            });
+
+            //listens for the click event on a marker to zoom the map when the marker is clicked
+            map.setZoom(8);
+            map.panTo(marker.getPosition());
+
+            history.pushState(null, null, "#"+marker.position.toUrlValue());
+        });
+
+        marker.addListener("dblclick", () => {
+            const doDelete = confirm("Are you sure you want to delete this marker?");
+            const latlngstr = JSON.stringify(marker.position.toJSON());
+            const deleted = marker;
+
+            if(doDelete) {
+                localStorage.removeItem(latlngstr);
+                markers = markers.filter(marker => marker !== deleted);
+                marker.setMap(null);
+                notes.style.visibility = "hidden";
+            }
+        });
+    }
+
+    //when click on map add marker
+    map.addListener('click', (event) => {
+        addMarker(event.latLng);
+    });
+
+    //add marker (automatically) function
+    function addMarker(coords) {
+        notes.style.visibility = "visible";
+        const bouncing = markers.find(marker => marker.animation == google.maps.Animation.BOUNCE);
+            if (bouncing) {
+                bouncing.setAnimation(null);
+        }
+
+        const marker = new google.maps.Marker({
+            position: coords,
+            map,
+            title: "Click to zoom, double click to delete",
+            icon: svgMarker,
+            label: `${markers.length + 1}`,
+            animation: google.maps.Animation.BOUNCE,
+        });
+
+        const noteWindow = new google.maps.InfoWindow();
+        const latlng = coords.toJSON()
+        const latlngstr = JSON.stringify(latlng);
+
+        //call reverse geocoding function to get adresses from coords to show in infowindow
+        geocodeLatLng(map, noteWindow, marker);
+        noteWindow.open({
+            anchor: marker,
+            shouldFocus: true,
+        });
+
+        //save markers coords to use as keys to get matching list of notes 
+        localStorage.setItem(latlngstr, []);
+        sessionStorage.setItem('helper', latlngstr);
+
+        //listens for the click event on a marker to show infowindow and to set current marker for getting match list of
+        marker.addListener("click", () => {
+            notes.style.visibility = "visible";
+            sessionStorage.setItem('helper', JSON.stringify(marker.position.toJSON()));
+
+            const bouncing = markers.find(marker => marker.animation == google.maps.Animation.BOUNCE);
+            if (bouncing) {
+                bouncing.setAnimation(null);
+            }
+            toggleBounce(marker);
+
+            //after click it also opens a note window
+            noteWindow.open({
+                anchor: marker,
+                shouldFocus: true,
+            });
+
+            //listens for the click event on a marker to zoom the map when the marker is clicked
+            map.setZoom(8);
+            map.panTo(marker.getPosition());
+            
+            history.pushState(null, null, "#"+ marker.position.toUrlValue());
+        });
+
+        marker.addListener("dblclick", () => {
+            const doDelete = confirm("Are you sure you want to delete this marker?");
+            const latlngstr = JSON.stringify(marker.position.toJSON());
+            const deleted = marker;
+
+            if(doDelete) {
+                localStorage.removeItem(latlngstr);
+                markers = markers.filter(marker => marker !== deleted);
+                marker.setMap(null);
+                notes.style.visibility = "hidden";
+            }
+        });
+
+        markers.push(marker);
+        //set the map center to actual marker
+        map.panTo(coords);
+        // creating url after adding new marker
+        history.pushState(null, null, "#"+ marker.position.toUrlValue());
+    }
+
+
+    // add event listeners for the buttons
+    const showBtn = $('#show-markers');
+    showBtn.click(showMarkers);
+    const hideBtn = $('#hide-markers');
+    hideBtn.click(hideMarkers);
+    const deleteBtn = $('#delete-markers');
+    deleteBtn.click(deleteMarkers);
+
+  /*  document.getElementById('show-markers').addEventListener('click', showMarkers);
+    document.getElementById('hide-markers').addEventListener('click', hideMarkers);
+    document.getElementById('delete-markers').addEventListener('click', deleteMarkers);*/
+
+    // Sets the map on all markers in the array.
+    function setMapOnAll(map) {
+        for (let i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
+
+    // Removes the markers from the map, but keeps them in the array.
+    function hideMarkers() {
+        setMapOnAll(null);
+    }
+
+    // Shows any markers currently in the array.
+    function showMarkers() {
+        setMapOnAll(map);
+    }
+
+    // Deletes all markers in the array by removing references to them.
+    function deleteMarkers() {
+        hideMarkers();
+        markers = [];
+        localStorage.clear();
+        //history.pushState(null, null, location.origin);
+        const doDelete = confirm("Are you sure you want to delete this note?");
+        if(doDelete) {
+            location.replace('http://127.0.0.1:5500/');
+        }
+    }
+
+
+    // Create the search box and link it to the UI element.
+    const input = document.getElementById("map-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+
+    //map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+        let markersSearch = [];
+        const places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markersSearch.forEach((marker) => {
+            marker.setMap(null);
+        });
+        markersSearch = [];
+
+        // For each place, get the icon, name and location.
+        const bounds = new google.maps.LatLngBounds();
+
+        places.forEach((place) => {
+            if (!place.geometry || !place.geometry.location) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+
+            const icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25),
+            };
+
+            // Create a marker for each place.
+            markersSearch.push(
+                new google.maps.Marker({
+                    map,
+                    icon,
+                    title: place.name,
+                    position: place.geometry.location,
+                })
+            );
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+}
+
+
+function geocodeLatLng(map, infowindow, marker) {
+    const geocoder = new google.maps.Geocoder();
+    const latlng = marker.position.toJSON(); 
+    geocoder
+      .geocode({
+        location: latlng
+    })
+    .then((response) => {
+        if (response.results[0]) {
+            
+          infowindow.setContent(response.results[0].formatted_address || "");
+        } else {
+          window.alert("No results found");
+        }
+    })
+    .catch((e) => window.alert("Geocoder failed due to: " + e));
+}
+
+function toggleBounce(marker) {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+/*
+// 10 seconds after the center of the map has changed, pan back to the
+// marker. Just an experiment :/
+map.addListener("center_changed", () => {
+    window.setTimeout(() => {
+        map.panTo(marker.getPosition());
+    }, 10000);
+});*/
